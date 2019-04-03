11aa1ebeaa55d42609ba34ced2add32a7df9461c
WayneNg <32226712+NgWayne@users.noreply.github.com>
2019-04-03 14:37:21 -0700

    Updated states. Changed so location is only parameter

--- SessionState.js
+++ SessionState.js
@@ -3,7 +3,10 @@ const db = require('./db/db.js');
 let RPM_THRESHOLD = 12;
 
 const STATE = {
+    RESET: 'Reset',
     NO_PRESENCE: 'No Presence',
+    DOOR_OPENED_START: "Door Opened: Start Session",
+    DOOR_OPENED_STOP: "Door Opened: Stop Session",
     MOVEMENT_NO_SESSION: "Movement with no active session",
     MOVEMENT: 'Movement',
 	BREATH_TRACKING: 'Breathing',
@@ -34,10 +37,13 @@ const incidentTypes = {
 					   
 class SessionState {
 
-	constructor(id, locationid, state, prev_state, phonenumber, rpm, x_state, movement_fast, movement_slow, door, motion, incidentType, notes, od_flag) {
-		this.id = id						// id is session number (?)
+    constructor(location){//id, locationid, state, prev_state, phonenumber, rpm, x_state, movement_fast, movement_slow, door, motion, incidentType, notes, od_flag) {
+		/*
+	    this.id = id						// id is session number (?)
 		this.locationid = locationid
+        */
 		this.location = location;
+        /*
 		this.rpm = rpm;
 		this.distance = distance;
 		this.mov_s = mov_s;
@@ -50,69 +56,101 @@ class SessionState {
 		this.incidentType = incidentType
 		this.notes = notes
 		this.od_flag = od_flag
+        */
 	}
 	
-	advanceChatbot(messageText) {	
+	advanceStateMachine() {	
 	
 	    let returnMessage;
-	    let prev_state = this.state;
+//	    let prev_state = states.state;
+	    let state;
 
-		switch (this.state) {
+	    let xethru = db.getLatestXeThruSensordata(this.location);
+	    let door = db.getLatestDoorSensorData(this.location);
+	    let motion = db.getLatestMotionSensorData(this.location);
+	    let states = db.getLatestLocationStatesData(this.location);
+	    let session = db.getMostRecentSession(this.location);
 
+		switch (states.state) {
+
+		    case STATE.RESET:
+		    {
+		        if(door.signal == false) { //Once the door has been closed
+		            state = STATE.NO_PRESENCE;
+		        }
+		    }
 		    case STATE.NO_PRESENCE:
 		    {
 		        //if not in no presence state, anymore 
-		        if (this.x_state != XETHRU_STATES.NO_MOVEMENT && this.door == true) {
-		            this.state = STATE.MOVEMENT;
+		        if (door.signal == true) {
+		            state = STATE.DOOR_OPENED;
 		        }
-		        else if (this.x_state != XETHRU_STATES.NO_MOVEMENT && this.door == false) {
-		            this.state = STATE.MOVEMENT_NO_SESSION;
+		        else if (xethru.state != XETHRU_STATES.NO_MOVEMENT && door.signal == false) {
+		            state = STATE.MOVEMENT_NO_SESSION;
+		        }
+		        break;
+		    }
+		    case STATE.DOOR_OPENED_START:
+		    {
+		        db.startSession(this.location);
+		        if (xethru.state != XETHRU_STATES.NO_MOVEMENT) {
+		            state = STATE.MOVEMENT;
 		        }
 		        break;
 		    }
 		    case STATE.MOVEMENT_NO_SESSION:
 		    {
-		        if (this.x_state == XETHRU_STATES.NO_MOVEMENT) {
-		            this.state = STATE.NO_PRESENCE;
+		        if (xethru.state == XETHRU_STATES.NO_MOVEMENT) {
+		            state = STATE.NO_PRESENCE;
 		        }
-		        else if (this.door == true) {
-		            this.state = STATE.MOVEMENT;
+		        else if (door.signal == true) {
+		            state = STATE.DOOR_OPENED_START;
 		        }
+		        break;
+		    }
+		    case STATE.DOOR_OPENED_STOP:
+		    {
+		        db.closeSession(this.location);
+		        state = STATE.RESET;
 		        break;
 		    }
 		    case STATE.MOVEMENT:
 		    {
 		        //if state is no movement, chenge to STATE_NO_PRESENCE
-		        if (this.x_state == XETHRU_STATES.NO_MOVEMENT && !this.motion) {
-		            this.state = STATE.NO_PRESENCE;
+		        if (xethru.state == XETHRU_STATES.NO_MOVEMENT && !motion.signal) {
+		            state = STATE.NO_PRESENCE;
 		        }
 		            //if in breathing state, change to that state
-		        else if (this.x_state == XETHRU_STATES.BREATHING) {
-		            this.state = STATE.BREATH_TRACKING;
+		        else if (xethru.state == XETHRU_STATES.BREATHING) {
+		            state = STATE.BREATH_TRACKING;
 		        }
-		        else if (this.mov_f == 0) {
-		            this.state = STATE.STILL;
+		        else if (xethru.mov_f == 0) {
+		            state = STATE.STILL;
 		        }
 
-		        if (this.od_flag == false && db.isOverdoseSuspected(location)) {
+		        if (door.signal == true) {
+		            state = STATE.DOOR_OPENED_STOP;
+		        }
+
+		        if (session.od_flag == false && db.isOverdoseSuspected(this.location)) {
 		            //startChatbot(location);
-		            this.state = STATE.SUSPECTED_OD;
+		            state = STATE.SUSPECTED_OD;
 		        }
 
 		        break;
 		    }
 		    case STATE.STILL:
 		    {
-		        if (this.x_state == XETHRU_STATES.BREATHING) {
-		            this.state = STATE.BREATH_TRACKING;
+		        if (xethru.state == XETHRU_STATES.BREATHING) {
+		            state = STATE.BREATH_TRACKING;
 		        }
-		        else if (this.mov_f > 0) {
-		            this.state = STATE.MOVEMENT;
+		        else if (xethru.mov_f > 0) {
+		            state = STATE.MOVEMENT;
 		        }
 
-		        if (this.od_flag == false && db.isOverdoseSuspected(location)) {
+		        if (session.od_flag == false && db.isOverdoseSuspected(location)) {
 		            //startChatbot(location);
-		            this.state = STATE.SUSPECTED_OD;
+		            state = STATE.SUSPECTED_OD;
 		        }
 
 		        break;
@@ -120,50 +158,63 @@ class SessionState {
 		    case STATE.BREATH_TRACKING:
 		    {
 		        //returns to movement if not in breathing state
-		        if (this.x_state != XETHRU_STATES.BREATHING) {
-		            this.state = STATE.MOVEMENT;
+		        if (xethru.state != XETHRU_STATES.BREATHING) {
+		            state = STATE.MOVEMENT;
 		        }
 		        //Check how many of the criteria are met to suspect an overdose
 
-		        if (od_flag == false && db.isOverdoseSuspected(location)) {
-		            startChatbot(location);
+		        if (session.od_flag == false && db.isOverdoseSuspected(location)) {
+		            //startChatbot(location);
+		            state = STATE.SUSPECTED_OD;
 		        }
 
 		        break;
 		    }
-			case STATE.SUSPECTED_OD:
+		    case STATE.SUSPECTED_OD:
+		    {
+		        //if there is a response for the chatbot, advance the state
+                /*
+		        if(message_received){
+		            this.state = STATE.STARTED;
+		            message_received = false;
+		        }
+                */
+		    }
+			case STATE.STARTED:
 			{
 				//Send an alert and something
-				this.state = STATE.WAITING_FOR_CATEGORY;
+				state = STATE.WAITING_FOR_CATEGORY;
 				//this.od_flag = 1;
 				returnMessage = "Respond with category";
 				break;
 			}
 			case STATE.WAITING_FOR_RESPONSE:
 			{
-				this.state = STATE.WAITING_FOR_CATEGORY;
+				state = STATE.WAITING_FOR_CATEGORY;
 				returnMessage = "Respond with category";
 				break;
 			}
 			case STATE.WAITING_FOR_CATEGORY:
 			{
 				let isValid = this.setIncidentType(messageText.trim());
-                this.state = isValid ? STATE.WAITING_FOR_DETAILS : STATE.WAITING_FOR_CATEGORY;
+                state = isValid ? STATE.WAITING_FOR_DETAILS : STATE.WAITING_FOR_CATEGORY;
                 returnMessage = isValid ? "Any additional notes" : "Invalid category, try again";
 
                 if (isValid) {
-                    db.updateSessionIncidentType(this);
+                    session.incidentType = messageText.trim();
+                    db.updateSessionIncidentType(session);
                 }
 
 				break;
 			}
 			case STATE.WAITING_FOR_DETAILS:
 			{
-				this.notes = messageText.trim();
-				this.state = STATE.COMPLETED;
+				session.notes = messageText.trim();
+				state = STATE.COMPLETED;
 				returnMessage = "Thanks";
 
-				db.updateSessionNotes(this);
+				db.updateSessionNotes(session);
+				db.closeSession(this.location);
 
 				break;
 			}
@@ -177,13 +228,15 @@ class SessionState {
 				returnMessage = "Error";
 				break;
 			}
-		}
-		
-		if (prev_state != this.state) { //new state is different than the previous state
-		    db.addStateMachineData(this.state, this.id, this.locationid);
-		}
+	    }
 
-		return returnMessage;
+		/*
+		if (prev_state != state) { //new state is different than the previous state
+		    db.addStateMachineData(state, session.id, this.location);
+		}
+        */
+
+		return this.state;
 	}
 
 	
