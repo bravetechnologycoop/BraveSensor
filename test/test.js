69e92f9788ea3c7d62b948fefacff11dae90ea97
Brave <brave@Veras-MacBook-Pro.local>
2019-04-10 15:14:35 -0700

    Fix indentation
    So that it is easier to read

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ let chai = require('chai');
 const expect = chai.expect;
 
 describe('test test', () => {
-		it('should always pass', () => {
-			expect(true).to.true;
-		});
+	it('should always pass', () => {
+		expect(true).to.true;
+	});
 });
