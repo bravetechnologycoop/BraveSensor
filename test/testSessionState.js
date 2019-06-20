4e9fc0607145e4c1f2311fc2f51cfe44a850d954
trisapeace <trisapeace@gmail.com>
2019-06-19 17:22:02 -0700

    Factor out mocking code in the tests
    - This makes it easier to set up each different test run and reduces copy-and-pasting

--- test/testSessionState.js
+++ test/testSessionState.js
@@ -2,35 +2,85 @@ let chai = require('chai');
 const expect = chai.expect;
 const STATE = require('./../SessionStateEnum.js');
 const XETHRU_STATE = require('./../SessionStateXethruEnum.js');
-
 const SessionState = require('./../SessionState.js');
 
-describe('test getNextState', () => {
-	it('when no initial state should return the RESET state', async () => {
-		let db = {
-			getLocationData: function(location) {
-				// initial state
-				return {};
-			},
-			getLatestXeThruSensordata: function(location) {
-				return {};
-			},
-			getLatestDoorSensordata: function(location) {
-				return {};
-			},
-			getLatestMotionSensordata: function(location) {
-				return {};
-			},
-			getLatestLocationStatesdata: function(location) {
-				return {};
-			},
-			addStateMachineData: function(state, location) {
-			}
+function setupDB(states = {}, door = {}, motion = {}, xethru = {}, location_data = {}) {
+	return {
+		getLatestLocationStatesdata: function(location) {
+			// initial state
+			return states;
+		},
+		getLatestXeThruSensordata: function(location) {
+			return xethru;
+		},
+		getLatestDoorSensordata: function(location) {
+			return door;
+		},
+		getLatestMotionSensordata: function(location) {
+			return motion;
+		},
+		getLocationData: function(location) {
+			return location_data;
+		},
+		addStateMachineData: function(state, location) {
 		}
-		let statemachine = new SessionState('TestLocation');
+	}
+}
 
-		let actualState = await statemachine.getNextState(db);
+describe('test getNextState', () => {
+	describe('when no inital state', () => {
+		it('should return the RESET state', async () => {
+			let db = setupDB();
+			let statemachine = new SessionState('TestLocation');
 
-		expect(actualState).to.equal(STATE.RESET);
+			let actualState = await statemachine.getNextState(db);
+
+			expect(actualState).to.equal(STATE.RESET);
+		});
+	});
+
+	describe('when inital state is NO_PRESENCE_NO_SESSION', () => {
+		it('and the door opens, should return the DOOR_OPENED_START state', async () => {
+			let db = setupDB(
+				states = {state: STATE.NO_PRESENCE_NO_SESSION},
+				door = {signal:"open"}
+			);
+			let statemachine = new SessionState('TestLocation');
+
+			let actualState = await statemachine.getNextState(db);
+
+			expect(actualState).to.equal(STATE.DOOR_OPENED_START);
+		});
+
+		it('and the door closes, should not change state', async () => {
+			let initialState = STATE.NO_PRESENCE_NO_SESSION;
+			let db = setupDB(
+				states = {state: initialState},
+				door = {signal: "closed"}
+			);
+			let statemachine = new SessionState('TestLocation');
+
+			let actualState = await statemachine.getNextState(db);
+
+			expect(actualState).to.equal(initialState);
+		});
+
+		it(', the motion sensor is active, the xethru detects movement, and the xethru movement is more than the movement threshold, should return the MOTION_DETECTED state', async () => {
+			let db = setupDB(
+				states = {state: STATE.NO_PRESENCE_NO_SESSION},
+				door = {},
+				motion = {signal: "active"},
+				xethru = {
+					state: XETHRU_STATE.MOVEMENT,
+					mov_f: 6
+				},
+				location_data = {mov_threshold: 5}
+			);
+			let statemachine = new SessionState('TestLocation');
+
+			let actualState = await statemachine.getNextState(db);
+
+			expect(actualState).to.equal(STATE.MOTION_DETECTED);
+		});
 	});
 });
