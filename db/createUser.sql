447bed91d0e0d4d2de5329e911d4ed67a1eed92e
MarioCimet <mariocimet@gmail.com>
2019-10-28 19:40:10 -0700

    added migration tracking and changed database operations so that script can be run multiple times

--- db/createUser.sql
+++ db/createUser.sql
@@ -1,17 +1,18 @@
-create or replace function f_create_user ( pv_username name
-                                         , pv_password text
-                                         ) returns boolean
-as $$
-declare
+DROP FUNCTION IF EXISTS f_create_user(text,text);
+
+
+create or replace function f_create_user (_username name, _password text) 
+returns boolean as $setup$
+  declare
   lb_return boolean := true;
   ln_count integer;
 begin
-  if ( pv_username is null )
+  if (_username is null )
   then
      raise warning 'Username must not be null';
      lb_return := false;
   end if;
-  if ( pv_password is null )
+  if (_password is null )
   then
      raise warning 'Password must not be null';
      lb_return := false;
@@ -21,7 +22,7 @@ begin
       select count(*)
         into ln_count
         from pg_user
-       where usename = pv_username;
+       where usename = _username;
   exception
       when no_data_found then
           -- ok, no user with this name is defined
@@ -32,11 +33,12 @@ begin
   end;
   if ( ln_count > 0 )
   then
-     raise warning 'The user "%" already exist', pv_username;
+     raise warning 'The user "%" already exist', _username;
      lb_return := false;
   else
-      execute 'create user '||pv_username||' with password '||''''||'pv_password'||'''';
+      execute 'create user '||_username||' with password '||''''||'_password'||'''';
   end if;
   return lb_return;
-end;
-$$ language plpgsql;
+end $setup$ language plpgsql;
+
+SELECT f_create_user(:username, :password);
