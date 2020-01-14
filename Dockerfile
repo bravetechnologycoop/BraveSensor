2f9194516d630949bfdf32d3516c71f59c9b27d2
Mario <mario@brave.coop>
2020-01-14 18:29:06 -0500

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM node:11
-RUN apt-get update
-RUN apt-get install -y postgresql postgresql-contrib
 RUN mkdir -p /home/node/ODetect-Backend-Local/node_modules && chown -R node:node /home/node/ODetect-Backend-Local
 WORKDIR /home/node/ODetect-Backend-Local
 COPY package*.json ./
--- index.js
+++ index.js
@@ -637,11 +637,12 @@ setInterval(async function () {
 }, 1000); // Set to transmit data every 1000 ms.
 
 
-let httpsOptions = {
-  key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
-  cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`)
-}
-server = https.createServer(httpsOptions, app).listen(port)
+// let httpsOptions = {
+//   key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem`),
+//   cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem`)
+// }
+// server = https.createServer(httpsOptions, app).listen(port)
+server = app.listen(port)
 console.log('ODetect brave server listening on port 443')
 
 // Socket.io server connection start
