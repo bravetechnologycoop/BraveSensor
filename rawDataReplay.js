91f4db0cdc4dc2544155f0dbda9bbef1c3c5ff85
Sampath Satti <sampathsatti@gmail.com>
2020-03-25 15:00:54 -0700

    Get Metrics to log the  number of sessions arising from replayed data

--- rawDataReplay.js
+++ rawDataReplay.js
@@ -1,5 +1,6 @@
 const db = require('./db/db.js');
 const pg = require('pg')
+let moment = require('moment');
 const Sentry = require('@sentry/node');
 require('dotenv').config();
 pgconnectionString = process.env.PG_TEST_CONNECTION_STRING
@@ -24,32 +25,72 @@ pool.on('error', (err, client) => {
 
 module.exports.replayData = async function replayData(){
     let events = await getRawDataForInterval();
+    let replayRunTime = await getServerTime();
+
     for(let i = 0; i<events.length; i++){
-    await sleep(1000);
-    console.log(events[i]);
-     sendRequestForRow(events[i])
+      await sleep(1000);
+      console.log(events[i]);
+      sendRequestForRow(events[i])
     }
+    
+    getMetrics(replayRunTime);
+    
+}
+
+// Replay metrics
+
+async function getMetrics(replayRunTime){
+  try{
+    const results = await pool.query('Select count (*) from sessions where start_time > $1', [replayRunTime]);
+    if(results == undefined){
+      return null;
+    }
+    else{
+      console.log(results.rows[0])
+      return results.rows; 
+    }
+  }
+  catch(e){
+    console.log(`Error running the Sessions query: ${e}`);
+  }
+}
+
+// Get current time on the server
+
+async function getServerTime(){
+  try{
+    const results = await pool.query('Select NOW()');
+    if(results == undefined){
+      return null;
+    }
+    else{
+      return results.rows[0].now; 
+    }
+  }
+  catch(e){
+    console.log(`Error getting time on database: ${e}`);
+  }
 }
 
 //Select a specific twenty minute interval
 
 
-  async function getRawDataForInterval(){
-    try{
-      const results = await pool.query("Select * from rawdata where published_at < (CURRENT_TIMESTAMP - interval '3 days') and published_at > ((CURRENT_TIMESTAMP - interval '3 days') - interval '20 minutes') order by published_at asc");
-      if(results == undefined){
-        return null;
-      }
-      else{
-        console.log(JSON.stringify(results.rows[0]))
-        return results.rows; 
-      }
+async function getRawDataForInterval(){
+  try{
+    const results = await pool.query("Select * from rawdata where published_at < (CURRENT_TIMESTAMP - interval '3 days') and published_at > ((CURRENT_TIMESTAMP - interval '3 days') - interval '2 minutes') order by published_at asc");
+    if(results == undefined){
+      return null;
     }
-    catch(e){
-      console.log(`Error running the getRawData query: ${e}`);
+    else{
+      console.log(JSON.stringify(results.rows[0]))
+      return results.rows; 
     }
-  
   }
+  catch(e){
+    console.log(`Error running the getRawData query: ${e}`);
+  }
+
+}
 
 //Turn an individual database row into an http request
 function sendRequestForRow(e){
