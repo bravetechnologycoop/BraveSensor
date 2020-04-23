845bd7e4fcdb6bb94715a704bbc8f986cdc8bd23
mario <mario@mario-home.hitronhub.home>
2020-04-23 15:26:20 -0700

    added setup and teardown

--- replayData.js
+++ replayData.js
@@ -2,12 +2,10 @@ process.argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
 });
 
-const db = require('./db/db.js');
 const pg = require('pg')
-let moment = require('moment');
-const Sentry = require('@sentry/node');
 require('dotenv').config();
 pgconnectionString = process.env.PG_TEST_CONNECTION_STRING
+applicationConnectionString = process.env.PG_CONNECTION_STRING
 
 
 let numLocations = process.argv[2];
@@ -25,6 +23,10 @@ const pool = new pg.Pool({
   connectionString: pgconnectionString
 })
 
+const appPool = new pg.Pool({
+  connectionString: applicationConnectionString
+})
+
 // 1114 is OID for timestamp in Postgres
 // return string as is
 pg.types.setTypeParser(1114, str => str);
@@ -33,22 +35,25 @@ pool.on('error', (err, client) => {
     console.error('unexpected database error:', err)
 })
 
+appPool.on('error', (err, client) => {
+  console.error('unexpected database error:', err)
+})
 
 replayData(numLocations, startTime, endTime, locationID);
 
 async function replayData(numLocations, startTime, endTime, locationID){
 
+
+  seedLocations(numLocations)
   console.log(numLocations);
   let events = await getRawDataForInterval(startTime, endTime, locationID);
   let replayRunTime = await getServerTime();
+  let delay = 1000/numLocations;
 
-  //Point to the application logic server
-
-  //Prime the application server to recieve new data
 
   //Replay Data
   for(let i = 0; i<events.length; i++){
-    await sleep(1000);
+    await sleep(delay);
     console.log(events[i]);
     for(let j = 1; j<numLocations; j++){
       console.log(j);
@@ -57,11 +62,19 @@ async function replayData(numLocations, startTime, endTime, locationID){
     }
   }
   getMetrics(replayRunTime);
-    
+
+  teardownDB()  
+}
+
+//Prepare database for test
+async function seedLocations(numLocations){
+  for(let i=1; i<numLocations + 1; i++){
+    let location = 'TestLocation'+i
+    seedLocation(location)
+  }
 }
 
 // Replay metrics
-
 async function getMetrics(replayRunTime){
   try{
     const results = await pool.query('Select count (*) from sessions where start_time > $1', [replayRunTime]);
@@ -79,7 +92,6 @@ async function getMetrics(replayRunTime){
 }
 
 // Get current time on the server
-
 async function getServerTime(){
   try{
     const results = await pool.query('Select NOW()');
@@ -96,8 +108,6 @@ async function getServerTime(){
 }
 
 //Select a specific twenty minute interval
-
-
 async function getRawDataForInterval(startTime, endTime, locationID){
   try{
     const results = await pool.query("Select * from rawdata where published_at > $1 and published_at < $2 and locationid = $3 order by published_at asc", [startTime, endTime, locationID]);
@@ -148,3 +158,73 @@ function sendRequestForRow(event, location){
 
     }
   }
+
+async function seedLocation(locationid){
+  appPool.query("INSERT INTO locations (deviceid, locationid, phonenumber) VALUES (0, $1, 'XeThru', '+4206666969')", [locationid], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("INSERT INTO door_sensordata (deviceid, locationid, devicetype, signal) VALUES (0, $1, 'Door', 'closed')", [locationid], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("INSERT INTO xethru (deviceid, locationid, devicetype, state, rpm, distance, mov_f, mov_s) VALUES (0, $1, 'XeThru', 3, 0, 0, 0, 0)", [locationid], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("INSERT INTO states (locationid, state) VALUES ($1, 'Reset')", [locationid], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("INSERT INTO sessions (locationid, end_time, od_flag, state) VALUES ($1, CURRENT_TIMESTAMP, 0, 'Reset')", [locationid], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+//Clear application database for next run
+async function teardownDB(){
+  appPool.query("DELETE FROM states", (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("DELETE FROM xethru", (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("DELETE FROM door_sensordata", (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("DELETE FROM sessions", (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  appPool.query("ALTER SEQUENCE sessions_sessionid_seq restart with 1", (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+
+
