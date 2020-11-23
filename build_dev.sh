2d69aa6e3af34474b893039bd592fb8c41528444
Mario Cimet <mario@brave.coop>
2020-11-23 12:42:03 -0800

    using latest git commit hash in dev contanier build script to avoid overwriting tags

--- build_dev.sh
+++ build_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 doctl registry login
-docker build . -t registry.digitalocean.com/odetect/odetect-dev
-docker push registry.digitalocean.com/odetect/odetect-dev
+docker build -t registry.digitalocean.com/odetect/odetect-dev:$(git log -1 --format=%h) .
+docker push registry.digitalocean.com/odetect/odetect-dev:$(git log -1 --format=%h)
