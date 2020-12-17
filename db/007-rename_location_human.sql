afb48ed570eef77dfac69ad3f90e9452e037a633
Marisa Doig <mcr.doig@gmail.com>
2020-12-17 19:56:44 +0000

    Rename migration to coordinate with Mario

--- db/007-rename_location_human.sql
+++ db/007-rename_location_human.sql
@@ -3,7 +3,7 @@ DO $migration$
     DECLARE lastSuccessfulMigrationId INT;
 BEGIN
     -- The migration ID of this file
-    migrationId := 8;
+    migrationId := 7;
 
     -- Get the migration ID of the last file to be successfully run
     SELECT MAX(id) INTO lastSuccessfulMigrationId
