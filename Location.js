cd251d522fa28efb03e567dc78680bec9548993d
risatronic <mcr.doig@gmail.com>
2021-01-14 15:45:39 -0800

    Remove weird commas from Location.js & Session.js

--- Location.js
+++ Location.js
@@ -16,21 +16,21 @@ class Location {
     xethruSentAlerts,
     xethruHeartbeatNumber
   ) {
-    ;(this.locationid = locationid),
-      (this.displayName = displayName),
-      (this.deviceid = deviceid),
-      (this.phonenumber = phonenumber),
-      (this.detectionzoneMin = detectionzoneMin),
-      (this.detectionzoneMax = detectionzoneMax),
-      (this.sensitivity = sensitivity),
-      (this.led = led),
-      (this.noisemap = noisemap),
-      (this.movThreshold = movThreshold),
-      (this.durationThreshold = durationThreshold),
-      (this.stillThreshold = stillThreshold),
-      (this.rpmThreshold = rpmThreshold),
-      (this.xethruSentAlerts = xethruSentAlerts),
-      (this.xethruHeartbeatNumber = xethruHeartbeatNumber)
+    this.locationid = locationid
+    this.displayName = displayName
+    this.deviceid = deviceid
+    this.phonenumber = phonenumber
+    this.detectionzoneMin = detectionzoneMin
+    this.detectionzoneMax = detectionzoneMax
+    this.sensitivity = sensitivity
+    this.led = led
+    this.noisemap = noisemap
+    this.movThreshold = movThreshold
+    this.durationThreshold = durationThreshold
+    this.stillThreshold = stillThreshold
+    this.rpmThreshold = rpmThreshold
+    this.xethruSentAlerts = xethruSentAlerts
+    this.xethruHeartbeatNumber = xethruHeartbeatNumber
   }
 }
 
--- Session.js
+++ Session.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-sequences */
-/* eslint-disable no-unused-expressions */
 class Session {
   constructor(
     locationid,
@@ -16,22 +14,20 @@ class Session {
     chatbotState,
     alertReason
   ) {
-    ;(this.locationid = locationid),
-      (this.startTime = startTime),
-      (this.endTime = endTime),
-      (this.odFlag = odFlag),
-      (this.state = state),
-      (this.phonenumber = phonenumber),
-      (this.notes = notes),
-      (this.incidentType = incidentType),
-      (this.sessionid = sessionid),
-      (this.duration = duration),
-      (this.stillCounter = stillCounter),
-      (this.chatbotState = chatbotState),
-      (this.alertReason = alertReason)
+    this.locationid = locationid
+    this.startTime = startTime
+    this.endTime = endTime
+    this.odFlag = odFlag
+    this.state = state
+    this.phonenumber = phonenumber
+    this.notes = notes
+    this.incidentType = incidentType
+    this.sessionid = sessionid
+    this.duration = duration
+    this.stillCounter = stillCounter
+    this.chatbotState = chatbotState
+    this.alertReason = alertReason
   }
 }
 
 module.exports = Session
-
-// it formatted it to this and then I still had to disable things to make it not have red lines everywhere...
