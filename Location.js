2178d265ab69844b0aa9fc44bb0b03f68d8c4b02
Marisa Doig <mcr.doig@gmail.com>
2021-01-15 22:27:46 +0000

    Remove incorrect commas noticed when trying out linting

--- Location.js
+++ Location.js
@@ -1,21 +1,23 @@
 class Location {
+    
     constructor(locationid, displayName, deviceid, phonenumber, detectionzoneMin, detectionzoneMax, sensitivity, led, noisemap, movThreshold, durationThreshold, stillThreshold, rpmThreshold, xethruSentAlerts, xethruHeartbeatNumber) {
-        this.locationid = locationid,
-        this.displayName = displayName,
-        this.deviceid = deviceid,
-        this.phonenumber = phonenumber,
-        this.detectionzoneMin = detectionzoneMin,
-        this.detectionzoneMax = detectionzoneMax,
-        this.sensitivity = sensitivity,
-        this.led = led,
-        this.noisemap = noisemap,
-        this.movThreshold = movThreshold,
-        this.durationThreshold = durationThreshold,
-        this.stillThreshold = stillThreshold,
-        this.rpmThreshold = rpmThreshold,
-        this.xethruSentAlerts = xethruSentAlerts,
+        this.locationid = locationid
+        this.displayName = displayName
+        this.deviceid = deviceid
+        this.phonenumber = phonenumber
+        this.detectionzoneMin = detectionzoneMin
+        this.detectionzoneMax = detectionzoneMax
+        this.sensitivity = sensitivity
+        this.led = led
+        this.noisemap = noisemap
+        this.movThreshold = movThreshold
+        this.durationThreshold = durationThreshold
+        this.stillThreshold = stillThreshold
+        this.rpmThreshold = rpmThreshold
+        this.xethruSentAlerts = xethruSentAlerts
         this.xethruHeartbeatNumber = xethruHeartbeatNumber
     }
+
 }
 
 module.exports = Location
