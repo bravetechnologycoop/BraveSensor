b82f268c6fd325c78cbf0e8f548afd390907b23e
Marisa Doig <mcr.doig@gmail.com>
2021-01-25 04:57:29 +0000

    Update StateMachine.js to conform, disabling rule was necessary on lines 46/47 and unsure why, something to look at

--- StateMachine.js
+++ StateMachine.js
@@ -26,46 +26,48 @@ class StateMachine {
     const latestDoor = moment(door.timestamp, 'x')
     const doorDelay = currentTime.diff(latestDoor)
 
-    if (states == undefined) {
+    if (states === undefined) {
       // In case the DB states table is empty create a RESET entry
       await redis.addStateMachineData(STATE.RESET, this.location)
       state = STATE.RESET
     } else {
+      let mov_s_avg
       state = states.state // Takes current state in case that non of the state conditions get meet for a state transition.
 
       switch (states.state) {
         case STATE.RESET:
           // Waits for the door to close before restarting the state machine
-          if (door.signal == DOOR_STATE.CLOSED) {
+          if (door.signal === DOOR_STATE.CLOSED) {
             state = STATE.NO_PRESENCE_NO_SESSION
           }
           break
         case STATE.NO_PRESENCE_NO_SESSION:
           // Checks the average XeThru Value over the last 15 seconds
-          var mov_f_avg = xethru_history.map(entry => entry.mov_f).reduce((a, b) => a + b) / xethru_history.length
-          var mov_s_avg = xethru_history.map(entry => entry.mov_s).reduce((a, b) => a + b) / xethru_history.length
+          // eslint-disable-next-line no-case-declarations
+          const mov_f_avg = xethru_history.map(entry => entry.mov_f).reduce((a, b) => a + b) / xethru_history.length
+          mov_s_avg = xethru_history.map(entry => entry.mov_s).reduce((a, b) => a + b) / xethru_history.length
 
           // Door opens
-          if (door.signal == DOOR_STATE.OPEN) {
+          if (door.signal === DOOR_STATE.OPEN) {
             state = STATE.DOOR_OPENED_START
           }
 
           // Waits for both the XeThru and motion sensor to be active
           // Removing the condition for motion sensor to be active to go into this loop since we don't have a motion sensor in this space
-          else if ((mov_f_avg > residual_mov_f || mov_s_avg > 55) && xethru.state != XETHRU_STATE.NO_MOVEMENT && doorDelay > DOOR_THRESHOLD_MILLIS) {
+          else if ((mov_f_avg > residual_mov_f || mov_s_avg > 55) && xethru.state !== XETHRU_STATE.NO_MOVEMENT && doorDelay > DOOR_THRESHOLD_MILLIS) {
             state = STATE.MOTION_DETECTED
           }
           break
         case STATE.DOOR_OPENED_START:
           // Waits in this state for a while before proceeding.
-          if (door.signal == DOOR_STATE.CLOSED) {
+          if (door.signal === DOOR_STATE.CLOSED) {
             if (doorDelay < DOOR_THRESHOLD_MILLIS) {
               state = STATE.DOOR_OPENED_START
             }
             if (
               doorDelay >= DOOR_THRESHOLD_MILLIS &&
               (xethru.mov_f > residual_mov_f || xethru.mov_s > 55) &&
-              xethru.state != XETHRU_STATE.NO_MOVEMENT
+              xethru.state !== XETHRU_STATE.NO_MOVEMENT
             ) {
               state = STATE.DOOR_CLOSED_START
             } else {
@@ -74,7 +76,7 @@ class StateMachine {
           }
           break
         case STATE.DOOR_CLOSED_START:
-          if (xethru.state == XETHRU_STATE.MOVEMENT || xethru.state == XETHRU_STATE.BREATHING || xethru.state == XETHRU_STATE.MOVEMENT_TRACKING) {
+          if (xethru.state === XETHRU_STATE.MOVEMENT || xethru.state === XETHRU_STATE.BREATHING || xethru.state === XETHRU_STATE.MOVEMENT_TRACKING) {
             state = STATE.MOVEMENT
           }
           break
@@ -85,48 +87,48 @@ class StateMachine {
           state = STATE.RESET
           break
         case STATE.MOVEMENT:
-          if (door.signal == DOOR_STATE.OPEN && session.od_flag == OD_FLAG_STATE.NO_OVERDOSE) {
+          if (door.signal === DOOR_STATE.OPEN && session.od_flag === OD_FLAG_STATE.NO_OVERDOSE) {
             state = STATE.DOOR_OPENED_CLOSE
           }
           // if in breathing state, change to that state
-          else if (xethru.state == XETHRU_STATE.BREATHING) {
+          else if (xethru.state === XETHRU_STATE.BREATHING) {
             state = STATE.BREATH_TRACKING
-          } else if (xethru.mov_f == 0 && xethru.state != XETHRU_STATE.NO_MOVEMENT) {
+          } else if (xethru.mov_f === 0 && xethru.state !== XETHRU_STATE.NO_MOVEMENT) {
             state = STATE.STILL
           }
 
-          if (session.od_flag == OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
+          if (session.od_flag === OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
             state = STATE.SUSPECTED_OD
           }
 
           break
         case STATE.STILL:
-          if (door.signal == DOOR_STATE.OPEN && session.od_flag == OD_FLAG_STATE.NO_OVERDOSE) {
+          if (door.signal === DOOR_STATE.OPEN && session.od_flag === OD_FLAG_STATE.NO_OVERDOSE) {
             state = STATE.DOOR_OPENED_CLOSE
-          } else if (xethru.state == XETHRU_STATE.BREATHING) {
+          } else if (xethru.state === XETHRU_STATE.BREATHING) {
             state = STATE.BREATH_TRACKING
           } else if (xethru.mov_f > 0) {
             state = STATE.MOVEMENT
           }
 
-          if (session.od_flag == OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
+          if (session.od_flag === OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
             state = STATE.SUSPECTED_OD
           }
 
           break
         case STATE.BREATH_TRACKING:
-          if (door.signal == DOOR_STATE.OPEN && session.od_flag == OD_FLAG_STATE.NO_OVERDOSE) {
+          if (door.signal === DOOR_STATE.OPEN && session.od_flag === OD_FLAG_STATE.NO_OVERDOSE) {
             state = STATE.DOOR_OPENED_CLOSE
-          } else if (xethru.state != XETHRU_STATE.BREATHING && xethru.mov_f == 0) {
+          } else if (xethru.state !== XETHRU_STATE.BREATHING && xethru.mov_f === 0) {
             state = STATE.STILL
           }
           // returns to movement if not in breathing state
-          else if (xethru.state != XETHRU_STATE.BREATHING) {
+          else if (xethru.state !== XETHRU_STATE.BREATHING) {
             state = STATE.MOVEMENT
           }
 
           // If the flag was originally false and the overdose criteria are met, an overdose is ssuspected and the flag is enabled.
-          if (session.od_flag == OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
+          if (session.od_flag === OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
             state = STATE.SUSPECTED_OD
           }
 
