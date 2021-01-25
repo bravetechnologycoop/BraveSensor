db6dc39fa83b60f43964ea8f2b429a274fb0b505
Marisa Doig <mcr.doig@gmail.com>
2021-01-25 05:25:34 +0000

    StateMachine.js conforms to linting and passes tests but had to disable eqeqeq rule for some lines as it was causing tests to fail & still causes unhandled promise rejection warnings

--- StateMachine.js
+++ StateMachine.js
@@ -26,18 +26,19 @@ class StateMachine {
     const latestDoor = moment(door.timestamp, 'x')
     const doorDelay = currentTime.diff(latestDoor)
 
-    if (states === undefined) {
+    // eslint-disable-next-line eqeqeq
+    if (states == undefined) {
       // In case the DB states table is empty create a RESET entry
       await redis.addStateMachineData(STATE.RESET, this.location)
       state = STATE.RESET
     } else {
-      let mov_s_avg
-      state = states.state // Takes current state in case that non of the state conditions get meet for a state transition.
+      state = states.state // Takes current state in case none of the state conditions get met for a state transition
 
       switch (states.state) {
         case STATE.RESET:
           // Waits for the door to close before restarting the state machine
-          if (door.signal === DOOR_STATE.CLOSED) {
+          // eslint-disable-next-line eqeqeq
+          if (door.signal == DOOR_STATE.CLOSED) {
             state = STATE.NO_PRESENCE_NO_SESSION
           }
           break
@@ -45,7 +46,8 @@ class StateMachine {
           // Checks the average XeThru Value over the last 15 seconds
           // eslint-disable-next-line no-case-declarations
           const mov_f_avg = xethru_history.map(entry => entry.mov_f).reduce((a, b) => a + b) / xethru_history.length
-          mov_s_avg = xethru_history.map(entry => entry.mov_s).reduce((a, b) => a + b) / xethru_history.length
+          // eslint-disable-next-line no-case-declarations
+          const mov_s_avg = xethru_history.map(entry => entry.mov_s).reduce((a, b) => a + b) / xethru_history.length
 
           // Door opens
           if (door.signal === DOOR_STATE.OPEN) {
@@ -60,14 +62,16 @@ class StateMachine {
           break
         case STATE.DOOR_OPENED_START:
           // Waits in this state for a while before proceeding.
-          if (door.signal === DOOR_STATE.CLOSED) {
+          // eslint-disable-next-line eqeqeq
+          if (door.signal == DOOR_STATE.CLOSED) {
             if (doorDelay < DOOR_THRESHOLD_MILLIS) {
               state = STATE.DOOR_OPENED_START
             }
             if (
               doorDelay >= DOOR_THRESHOLD_MILLIS &&
               (xethru.mov_f > residual_mov_f || xethru.mov_s > 55) &&
-              xethru.state !== XETHRU_STATE.NO_MOVEMENT
+              // eslint-disable-next-line eqeqeq
+              xethru.state != XETHRU_STATE.NO_MOVEMENT
             ) {
               state = STATE.DOOR_CLOSED_START
             } else {
@@ -87,31 +91,38 @@ class StateMachine {
           state = STATE.RESET
           break
         case STATE.MOVEMENT:
-          if (door.signal === DOOR_STATE.OPEN && session.od_flag === OD_FLAG_STATE.NO_OVERDOSE) {
+          // eslint-disable-next-line eqeqeq
+          if (door.signal == DOOR_STATE.OPEN && session.od_flag == OD_FLAG_STATE.NO_OVERDOSE) {
             state = STATE.DOOR_OPENED_CLOSE
           }
           // if in breathing state, change to that state
-          else if (xethru.state === XETHRU_STATE.BREATHING) {
+          // eslint-disable-next-line eqeqeq
+          else if (xethru.state == XETHRU_STATE.BREATHING) {
             state = STATE.BREATH_TRACKING
-          } else if (xethru.mov_f === 0 && xethru.state !== XETHRU_STATE.NO_MOVEMENT) {
+            // eslint-disable-next-line eqeqeq
+          } else if (xethru.mov_f == 0 && xethru.state != XETHRU_STATE.NO_MOVEMENT) {
             state = STATE.STILL
           }
 
-          if (session.od_flag === OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
+          // eslint-disable-next-line eqeqeq
+          if (session.od_flag == OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
             state = STATE.SUSPECTED_OD
           }
 
           break
         case STATE.STILL:
-          if (door.signal === DOOR_STATE.OPEN && session.od_flag === OD_FLAG_STATE.NO_OVERDOSE) {
+          // eslint-disable-next-line eqeqeq
+          if (door.signal == DOOR_STATE.OPEN && session.od_flag == OD_FLAG_STATE.NO_OVERDOSE) {
             state = STATE.DOOR_OPENED_CLOSE
-          } else if (xethru.state === XETHRU_STATE.BREATHING) {
+            // eslint-disable-next-line eqeqeq
+          } else if (xethru.state == XETHRU_STATE.BREATHING) {
             state = STATE.BREATH_TRACKING
           } else if (xethru.mov_f > 0) {
             state = STATE.MOVEMENT
           }
 
-          if (session.od_flag === OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
+          // eslint-disable-next-line eqeqeq
+          if (session.od_flag == OD_FLAG_STATE.NO_OVERDOSE && (await db.isOverdoseSuspected(xethru, session, location_data))) {
             state = STATE.SUSPECTED_OD
           }
 
