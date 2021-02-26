7accc9cbfa7f3642101680ff1a81b664c8c16d05
Marisa Doig <mcr.doig@gmail.com>
2021-02-26 22:50:23 +0000

    Disabled some things similar to in Buttons repo, see comments

--- BraveAlerterConfigurator.js
+++ BraveAlerterConfigurator.js
@@ -1,110 +1,109 @@
+/* eslint-disable no-return-await */
+/* eslint-disable class-methods-use-this */
 // In-house dependencies
 const { BraveAlerter, AlertSession, ALERT_STATE, helpers } = require('brave-alert-lib')
 const db = require('./db/db.js')
 const redis = require('./db/redis.js')
 
-const incidentTypes = [
-    'No One Inside',
-    'Person responded',
-    'Overdose',
-    'None of the above'
-]
+const incidentTypes = ['No One Inside', 'Person responded', 'Overdose', 'None of the above']
 
 const incidentTypeKeys = ['1', '2', '3', '4']
 
 class BraveAlerterConfigurator {
-    constructor(startTimes) {
-        this.startTimes = startTimes
+  constructor(startTimes) {
+    this.startTimes = startTimes
+  }
+
+  createBraveAlerter() {
+    return new BraveAlerter(
+      this.getAlertSession.bind(this),
+      this.getAlertSessionByPhoneNumber.bind(this),
+      this.alertSessionChangedCallback.bind(this),
+      false,
+      this.getReturnMessage.bind(this),
+    )
+  }
+
+  async buildAlertSession(session) {
+    const locationData = await db.getLocationData(session.locationid)
+
+    const alertSession = new AlertSession(
+      session.sessionid,
+      session.chatbot_state,
+      session.incidenttype,
+      undefined,
+      `An alert to check on the washroom at ${locationData.display_name} was not responded to. Please check on it`,
+      locationData.phonenumber,
+      incidentTypeKeys,
+      incidentTypes,
+    )
+
+    return alertSession
+  }
+
+  async getAlertSession(sessionId) {
+    const session = await db.getSessionWithSessionId(sessionId)
+
+    return await this.buildAlertSession(session)
+  }
+
+  async getAlertSessionByPhoneNumber(phoneNumber) {
+    const session = await db.getMostRecentSessionPhone(phoneNumber)
+
+    return await this.buildAlertSession(session)
+  }
+
+  async alertSessionChangedCallback(alertSession) {
+    const incidentType = incidentTypes[incidentTypeKeys.indexOf(alertSession.incidentCategoryKey)]
+
+    const client = await db.beginTransaction()
+
+    const session = await db.getSessionWithSessionId(alertSession.sessionId, client)
+    await db.saveAlertSession(alertSession.alertState, incidentType, alertSession.sessionId, client)
+
+    const locationId = session.locationid
+
+    if (alertSession.alertState === ALERT_STATE.COMPLETED) {
+      // Closes the session, sets the session state to RESET
+      await db.closeSession(session.sessionid, client) // Adds the end_time to the latest open session from the LocationID
+      helpers.log(`Session at ${locationId} was closed successfully.`)
+      this.startTimes[locationId] = null // Stops the session timer for this location
+      redis.addStateMachineData('Reset', locationId)
     }
 
-    createBraveAlerter() {
-        return new BraveAlerter(
-            this.getAlertSession.bind(this),
-            this.getAlertSessionByPhoneNumber.bind(this),
-            this.alertSessionChangedCallback.bind(this),
-            false,
-            this.getReturnMessage.bind(this),
-        )
-    }
+    await db.commitTransaction(client)
+  }
 
-    async buildAlertSession(session) {
-        let locationData = await db.getLocationData(session.locationid)
+  getReturnMessage(fromAlertState, toAlertState) {
+    let returnMessage
 
-        let alertSession = new AlertSession(
-            session.sessionid,
-            session.chatbot_state,
-            session.incidenttype,
-            undefined,
-            `An alert to check on the washroom at ${locationData.display_name} was not responded to. Please check on it`,
-            locationData.phonenumber,
-            incidentTypeKeys,
-            incidentTypes,
-        )
+    switch (fromAlertState) {
+      case ALERT_STATE.STARTED:
+      case ALERT_STATE.WAITING_FOR_REPLY:
+        returnMessage =
+          'Please respond with the number corresponding to the incident. \n1: No One Inside\n2: Person Responded\n3: Overdose\n4: None of the Above'
+        break
 
-        return alertSession
-    }
-
-    async getAlertSession(sessionId) {
-        let session = await db.getSessionWithSessionId(sessionId)
-
-        return await this.buildAlertSession(session)
-    }
-
-    async getAlertSessionByPhoneNumber(phoneNumber) {
-        let session = await db.getMostRecentSessionPhone(phoneNumber)
-
-        return await this.buildAlertSession(session)
-    }
-
-    async alertSessionChangedCallback(alertSession) {
-        const incidentType = incidentTypes[incidentTypeKeys.indexOf(alertSession.incidentCategoryKey)]
-        
-        const client = await db.beginTransaction()
-
-        const session = await db.getSessionWithSessionId(alertSession.sessionId, client)
-        await db.saveAlertSession(alertSession.alertState, incidentType, alertSession.sessionId, client)
-        
-        const locationId = session.locationid
-
-        if (alertSession.alertState === ALERT_STATE.COMPLETED) {
-            // Closes the session, sets the session state to RESET
-            await db.closeSession(session.sessionid, client) // Adds the end_time to the latest open session from the LocationID
-            helpers.log(`Session at ${locationId} was closed successfully.`)
-            this.startTimes[locationId] = null // Stops the session timer for this location
-            redis.addStateMachineData('Reset', locationId)
+      case ALERT_STATE.WAITING_FOR_CATEGORY:
+        if (toAlertState === ALERT_STATE.WAITING_FOR_CATEGORY) {
+          returnMessage =
+            'Invalid category, please try again\n\nPlease respond with the number corresponding to the incident. \n1: No One Inside\n2: Person Responded\n3: Overdose\n4: None of the Above'
+        } else if (toAlertState === ALERT_STATE.COMPLETED) {
+          returnMessage = 'Thank you!'
         }
+        break
 
-        await db.commitTransaction(client)
+      case ALERT_STATE.COMPLETED:
+        returnMessage = 'Thank you'
+        break
+
+      default:
+        returnMessage = 'Error: No active chatbot found'
+        break
     }
 
-    getReturnMessage(fromAlertState, toAlertState) {
-        let returnMessage
-
-        switch(fromAlertState) {
-            case ALERT_STATE.STARTED:
-            case ALERT_STATE.WAITING_FOR_REPLY:
-                returnMessage = 'Please respond with the number corresponding to the incident. \n1: No One Inside\n2: Person Responded\n3: Overdose\n4: None of the Above'
-                break
-
-            case ALERT_STATE.WAITING_FOR_CATEGORY:
-                if (toAlertState === ALERT_STATE.WAITING_FOR_CATEGORY) {
-                    returnMessage = 'Invalid category, please try again\n\nPlease respond with the number corresponding to the incident. \n1: No One Inside\n2: Person Responded\n3: Overdose\n4: None of the Above'
-                } else if (toAlertState === ALERT_STATE.COMPLETED) {
-                    returnMessage = 'Thank you!'
-                }
-                break
-
-            case ALERT_STATE.COMPLETED:
-                returnMessage = 'Thank you'
-                break
-
-            default:
-                returnMessage = 'Error: No active chatbot found'
-                break
-        }
-
-        return returnMessage
-    }
+    return returnMessage
+  }
 }
 
 module.exports = BraveAlerterConfigurator
