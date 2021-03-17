3cd2d9c6a6028f20b29fbe3042a7518f3a919d3e
trisapeace <theresa@brave.coop>
2021-03-16 18:07:10 -0700

    Only build and push Docker container if it isn't already in the registry
    - Since our tags are based on the git commit number, if the tag already
      exists in the registry, then the exact code has already been pushed
      and doesn't need to be done again.
    - This will save us time (by not building and pushing when we don't have
      to) and it will decrease the number of times that we pull from Docker
      Hub (which as a rate limit of 100 pulls every 6 hours).

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 doctl registry login
-docker build -t registry.digitalocean.com/odetect/odetect-prod:$(git log -1 --format=%h) .
-docker push registry.digitalocean.com/odetect/odetect-prod:$(git log -1 --format=%h)
+
+# Check if the repository already has a container with this tag
+if ! [[ $(doctl registry repository list-tags --format Tag --no-header odetect-prod | grep $(git log -1 --format=%h)) ]]; then
+    docker build -t registry.digitalocean.com/odetect/odetect-prod:$(git log -1 --format=%h) .
+    docker push registry.digitalocean.com/odetect/odetect-prod:$(git log -1 --format=%h)
+fi
