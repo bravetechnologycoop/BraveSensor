bravetechnologycoop/BraveSensor-Server

Add `GET /alert/historicAlerts` endpoint
- For use by the Alert App

BraveAlerterConfigurator.js
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
 // In-house dependencies
-const { BraveAlerter, AlertSession, ALERT_STATE, helpers, Location, SYSTEM } = require('brave-alert-lib')
+const { BraveAlerter, AlertSession, ALERT_STATE, helpers, HistoricAlert, Location, SYSTEM } = require('brave-alert-lib')
 const db = require('./db/db')
 
 const incidentTypes = ['No One Inside', 'Person responded', 'Overdose', 'None of the above']
@@ -108,8 +108,21 @@ class BraveAlerterConfigurator {
     return new Location(locations[0].locationid, SYSTEM.SENSOR)
   }
 
-  async getHistoricAlertsByAlertApiKey() {
-    return null
+  createHistoricAlertFromRow(row) {
+    return new HistoricAlert(row.id, row.display_name, row.incident_type, row.alert_type, null, row.created_at, row.responded_at)
+  }
+
+  // Historic Alerts are those with status "Completed" or that were last updated longer ago than the SUBSEQUENT_ALERT_MESSAGE_THRESHOLD
+  async getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts) {
+    const maxTimeAgoInMillis = helpers.getEnvVar('SUBSEQUENT_ALERT_MESSAGE_THRESHOLD') * 1000
+
+    const historicAlerts = await db.getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts, maxTimeAgoInMillis)
+
+    if (!Array.isArray(historicAlerts)) {
+      return null
+    }
+
+    return historicAlerts.map(this.createHistoricAlertFromRow)
   }
 
   getReturnMessage(fromAlertState, toAlertState) {

db/db.js
@@ -276,6 +276,35 @@ async function saveSession(session, clientParam) {
   }
 }
 
+async function getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts, maxTimeAgoInMillis, clientParam) {
+  try {
+    // Historic Alerts are those with status "Completed" or that were last updated longer ago than the SESSION_RESET_TIMEOUT
+    const results = await runQuery(
+      'getHistoricAlertsByAlertApiKey',
+      `
+      SELECT s.id, l.display_name, s.incident_type, s.alert_type, s.created_at, s.responded_at
+      FROM sessions AS s
+      LEFT JOIN locations AS l ON s.locationid = l.locationid
+      WHERE l.alert_api_key = $1
+      AND (
+        s.chatbot_state = $2
+        OR s.updated_at < now() - $3 * INTERVAL '1 millisecond'
+      )
+      ORDER BY s.created_at DESC
+      LIMIT $4
+      `,
+      [alertApiKey, ALERT_STATE.COMPLETED, maxTimeAgoInMillis, maxHistoricAlerts],
+      clientParam,
+    )
+
+    return results.rows
+  } catch (err) {
+    helpers.log(JSON.stringify(err))
+  }
+
+  return null
+}
+
 // Retrieves the data from the locations table for a given location
 async function getLocationData(locationid, clientParam) {
   try {
@@ -526,6 +555,7 @@ module.exports = {
   getMostRecentSession,
   getSessionWithSessionId,
   getHistoryOfSessions,
+  getHistoricAlertsByAlertApiKey,
   getUnrespondedSessionWithLocationId,
   createSession,
   saveSession,

test/unit/BraveAlerterConfiguratorTest/getHistoricAlertsByAlertApiKeyTest.js
@@ -1,0 +1,119 @@
+// Third-party dependencies
+const { expect, use } = require('chai')
+const { afterEach, beforeEach, describe, it } = require('mocha')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+// In-house dependencies
+const { helpers, HistoricAlert, ALERT_TYPE } = require('brave-alert-lib')
+const db = require('../../../db/db')
+const BraveAlerterConfigurator = require('../../../BraveAlerterConfigurator')
+
+// Configure Chai
+use(sinonChai)
+
+const sandbox = sinon.createSandbox()
+
+describe('BraveAlerterConfigurator.js unit tests: getHistoricAlertsByAlertApiKey and createHistoricAlertFromRow', () => {
+  beforeEach(() => {
+    this.maxTimeAgoInMillis = 60000
+    sandbox
+      .stub(helpers, 'getEnvVar')
+      .withArgs('SUBSEQUENT_ALERT_MESSAGE_THRESHOLD')
+      .returns(this.maxTimeAgoInMillis / 1000)
+
+    const braveAlerterConfigurator = new BraveAlerterConfigurator()
+    this.braveAlerter = braveAlerterConfigurator.createBraveAlerter()
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('if there is a single result from db.getHistoricAlertsByAlertApiKey, returns an array containing a single HistoricAlert object with the returned data', async () => {
+    const results = {
+      id: 'id',
+      display_name: 'displayName',
+      incident_type: 'incidentType',
+      alert_type: ALERT_TYPE.SENSOR_DURATION,
+      created_at: new Date('2020-01-20T10:10:10.000Z'),
+      responded_at: new Date('2020-01-20T10:12:40.000Z'),
+    }
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns([results])
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.eql([
+      new HistoricAlert(results.id, results.display_name, results.incident_type, results.alert_type, null, results.created_at, results.responded_at),
+    ])
+  })
+
+  it('if there are multiple results from db.getHistoricAlertsByAlertApiKey, returns an array containing the HistoricAlert objects with the returned data', async () => {
+    const results1 = {
+      id: 'id1',
+      display_name: 'displayName1',
+      incident_type: 'incidentType1',
+      alert_type: ALERT_TYPE.SENSOR_DURATION,
+      created_at: new Date('2020-01-20T10:10:10.000Z'),
+      responded_at: new Date('2020-01-20T10:12:40.000Z'),
+    }
+    const results2 = {
+      id: 'id2',
+      display_name: 'displayName2',
+      incident_type: 'incidentType2',
+      alert_type: ALERT_TYPE.SENSOR_NO_MOTION,
+      created_at: new Date('2019-02-20T09:10:10.000Z'),
+      responded_at: new Date('2019-02-20T09:12:40.000Z'),
+    }
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns([results1, results2])
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.eql([
+      new HistoricAlert(
+        results1.id,
+        results1.display_name,
+        results1.incident_type,
+        results1.alert_type,
+        null,
+        results1.created_at,
+        results1.responded_at,
+      ),
+      new HistoricAlert(
+        results2.id,
+        results2.display_name,
+        results2.incident_type,
+        results2.alert_type,
+        null,
+        results2.created_at,
+        results2.responded_at,
+      ),
+    ])
+  })
+
+  it('if there no results from db.getHistoricAlertsByAlertApiKey, returns an empty array', async () => {
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns([])
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.eql([])
+  })
+
+  it('if db.getHistoricAlertsByAlertApiKey returns a non-array, returns null', async () => {
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns()
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.be.null
+  })
+
+  it('db.getHistoricAlertsByAlertApiKey is called with the given alertApiKey, the given maxHistoricAlerts, and the SUBSEQUENT_ALERT_MESSAGE_THRESHOLD from .env', async () => {
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns()
+
+    const alertApiKey = 'alertApiKey'
+    const maxHistoricAlerts = 'maxHistoricAlerts'
+    await this.braveAlerter.getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts)
+
+    expect(db.getHistoricAlertsByAlertApiKey).to.be.calledWith(alertApiKey, maxHistoricAlerts, this.maxTimeAgoInMillis)
+  })
+})

bravetechnologycoop/BraveSensor

BraveAlerterConfigurator.js
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
 // In-house dependencies
-const { BraveAlerter, AlertSession, CHATBOT_STATE, helpers, Location, SYSTEM } = require('brave-alert-lib')
+const { BraveAlerter, AlertSession, CHATBOT_STATE, helpers, HistoricAlert, Location, SYSTEM } = require('brave-alert-lib')
 const db = require('./db/db')
 
 const incidentTypes = ['No One Inside', 'Person responded', 'Overdose', 'None of the above']
@@ -109,8 +109,21 @@ class BraveAlerterConfigurator {
     return new Location(locations[0].locationid, SYSTEM.SENSOR)
   }
 
-  async getHistoricAlertsByAlertApiKey() {
-    return null
+  createHistoricAlertFromRow(row) {
+    return new HistoricAlert(row.id, row.display_name, row.incident_type, row.alert_type, null, row.created_at, row.responded_at)
+  }
+
+  // Historic Alerts are those with status "Completed" or that were last updated longer ago than the SESSION_RESET_THRESHOLD
+  async getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts) {
+    const maxTimeAgoInMillis = helpers.getEnvVar('SESSION_RESET_THRESHOLD')
+
+    const historicAlerts = await db.getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts, maxTimeAgoInMillis)
+
+    if (!Array.isArray(historicAlerts)) {
+      return null
+    }
+
+    return historicAlerts.map(this.createHistoricAlertFromRow)
   }
 
   getReturnMessage(fromAlertState, toAlertState) {

db/db.js
@@ -276,6 +276,37 @@ async function saveSession(session, clientParam) {
   }
 }
 
+async function getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts, maxTimeAgoInMillis, clientParam) {
+  try {
+    // Historic Alerts are those with status "Completed" or that were last updated longer ago than the SESSION_RESET_TIMEOUT
+    const results = await runQuery(
+      'getHistoricAlertsByAlertApiKey',
+      `
+      SELECT s.id, l.display_name, s.incident_type, s.alert_type, s.created_at, s.responded_at
+      FROM sessions AS s
+      LEFT JOIN locations AS l ON s.locationid = l.locationid
+      WHERE l.alert_api_key = $1
+      AND (
+        s.chatbot_state = $2
+        OR s.updated_at < now() - $3 * INTERVAL '1 millisecond'
+      )
+      ORDER BY s.created_at DESC
+      LIMIT $4
+      `,
+      [alertApiKey, CHATBOT_STATE.COMPLETED, maxTimeAgoInMillis, maxHistoricAlerts],
+      clientParam,
+    )
+
+    if (results === undefined) {
+      return null
+    }
+
+    return results.rows
+  } catch (err) {
+    helpers.log(JSON.stringify(err))
+  }
+}
+
 // Retrieves the data from the locations table for a given location
 async function getLocationData(locationid, clientParam) {
   try {
@@ -526,6 +557,7 @@ module.exports = {
   getMostRecentSession,
   getSessionWithSessionId,
   getHistoryOfSessions,
+  getHistoricAlertsByAlertApiKey,
   getUnrespondedSessionWithLocationId,
   createSession,
   saveSession,

test/unit/BraveAlerterConfiguratorTest/getHistoricAlertsByAlertApiKeyTest.js
@@ -1,0 +1,116 @@
+// Third-party dependencies
+const { expect, use } = require('chai')
+const { afterEach, beforeEach, describe, it } = require('mocha')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+// In-house dependencies
+const { helpers, HistoricAlert, ALERT_TYPE } = require('brave-alert-lib')
+const db = require('../../../db/db')
+const BraveAlerterConfigurator = require('../../../BraveAlerterConfigurator')
+
+// Configure Chai
+use(sinonChai)
+
+const sandbox = sinon.createSandbox()
+
+describe('BraveAlerterConfigurator.js unit tests: getHistoricAlertsByAlertApiKey and createHistoricAlertFromRow', () => {
+  beforeEach(() => {
+    this.maxTimeAgoInMillis = 60000
+    sandbox.stub(helpers, 'getEnvVar').withArgs('SESSION_RESET_THRESHOLD').returns(this.maxTimeAgoInMillis)
+
+    const braveAlerterConfigurator = new BraveAlerterConfigurator()
+    this.braveAlerter = braveAlerterConfigurator.createBraveAlerter()
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('if there is a single result from db.getHistoricAlertsByAlertApiKey, returns an array containing a single HistoricAlert object with the returned data', async () => {
+    const results = {
+      id: 'id',
+      display_name: 'displayName',
+      incident_type: 'incidentType',
+      alert_type: ALERT_TYPE.SENSOR_DURATION,
+      created_at: new Date('2020-01-20T10:10:10.000Z'),
+      responded_at: new Date('2020-01-20T10:12:40.000Z'),
+    }
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns([results])
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.eql([
+      new HistoricAlert(results.id, results.display_name, results.incident_type, results.alert_type, null, results.created_at, results.responded_at),
+    ])
+  })
+
+  it('if there are multiple results from db.getHistoricAlertsByAlertApiKey, returns an array containing the HistoricAlert objects with the returned data', async () => {
+    const results1 = {
+      id: 'id1',
+      display_name: 'displayName1',
+      incident_type: 'incidentType1',
+      alert_type: ALERT_TYPE.SENSOR_DURATION,
+      created_at: new Date('2020-01-20T10:10:10.000Z'),
+      responded_at: new Date('2020-01-20T10:12:40.000Z'),
+    }
+    const results2 = {
+      id: 'id2',
+      display_name: 'displayName2',
+      incident_type: 'incidentType2',
+      alert_type: ALERT_TYPE.SENSOR_NO_MOTION,
+      created_at: new Date('2019-02-20T09:10:10.000Z'),
+      responded_at: new Date('2019-02-20T09:12:40.000Z'),
+    }
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns([results1, results2])
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.eql([
+      new HistoricAlert(
+        results1.id,
+        results1.display_name,
+        results1.incident_type,
+        results1.alert_type,
+        null,
+        results1.created_at,
+        results1.responded_at,
+      ),
+      new HistoricAlert(
+        results2.id,
+        results2.display_name,
+        results2.incident_type,
+        results2.alert_type,
+        null,
+        results2.created_at,
+        results2.responded_at,
+      ),
+    ])
+  })
+
+  it('if there no results from db.getHistoricAlertsByAlertApiKey, returns an empty array', async () => {
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns([])
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.eql([])
+  })
+
+  it('if db.getHistoricAlertsByAlertApiKey returns a non-array, returns null', async () => {
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns()
+
+    const historicAlerts = await this.braveAlerter.getHistoricAlertsByAlertApiKey('alertApiKey', 'maxHistoricAlerts')
+
+    expect(historicAlerts).to.be.null
+  })
+
+  it('db.getHistoricAlertsByAlertApiKey is called with the given alertApiKey, the given maxHistoricAlerts, and the SESSION_RESET_THRESHOLD from .env', async () => {
+    sandbox.stub(db, 'getHistoricAlertsByAlertApiKey').returns()
+
+    const alertApiKey = 'alertApiKey'
+    const maxHistoricAlerts = 'maxHistoricAlerts'
+    await this.braveAlerter.getHistoricAlertsByAlertApiKey(alertApiKey, maxHistoricAlerts)
+
+    expect(db.getHistoricAlertsByAlertApiKey).to.be.calledWith(alertApiKey, maxHistoricAlerts, this.maxTimeAgoInMillis)
+  })
+})