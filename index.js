bravetechnologycoop/BraveSensor-Server

Use alert_type_enum in DB and from Alert Lib instead of strings
- To better conform with our desired shared DB schema
- So that we don't need to translate between the AlertReasonEnum.js
  values here and the alertTypeEnum.js values in BraveAlertLib

index.js
@@ -11,8 +11,7 @@ const Mustache = require('mustache')
 const Validator = require('express-validator')
 
 // In-house dependencies
-const { helpers } = require('brave-alert-lib')
-const ALERT_REASON = require('./AlertReasonEnum')
+const { ALERT_TYPE, helpers } = require('brave-alert-lib')
 const redis = require('./db/redis')
 const db = require('./db/db')
 const StateMachine = require('./stateMachine/StateMachine')
@@ -108,8 +107,20 @@ async function generateCalculatedTimeDifferenceString(timeToCompare) {
   return returnString
 }
 
-async function handleAlert(location, alertReason) {
-  helpers.log(`Alert for: ${location.locationid} Display Name: ${location.displayName} CoreID: ${location.radarCoreId}`)
+function getAlertTypeDisplayName(alertType) {
+  let displayName = ''
+  if (alertType === ALERT_TYPE.SENSOR_DURATION) {
+    displayName = 'Duration'
+  } else if (alertType === ALERT_TYPE.SENSOR_NO_MOTION) {
+    displayName = 'Stillness'
+  }
+
+  return displayName
+}
+
+async function handleAlert(location, alertType) {
+  const alertTypeDisplayName = getAlertTypeDisplayName(alertType)
+  helpers.log(`${alertTypeDisplayName} Alert for: ${location.locationid} Display Name: ${location.displayName} CoreID: ${location.radarCoreId}`)
 
   let client
 
@@ -123,12 +134,12 @@ async function handleAlert(location, alertReason) {
     const currentTime = await db.getCurrentTime(client)
 
     if (currentSession === null || currentTime - currentSession.updatedAt >= helpers.getEnvVar('SESSION_RESET_THRESHOLD')) {
-      const newSession = await db.createSession(location.locationid, location.responderPhoneNumber, alertReason, client)
+      const newSession = await db.createSession(location.locationid, location.responderPhoneNumber, alertType, client)
       const alertInfo = {
         sessionId: newSession.id,
         toPhoneNumber: location.responderPhoneNumber,
         fromPhoneNumber: location.twilioNumber,
-        message: `This is a ${alertReason} alert. Please check on the bathroom at ${location.displayName}. Please respond with 'ok' once you have checked on it.`,
+        message: `This is a ${alertTypeDisplayName} alert. Please check on the bathroom at ${location.displayName}. Please respond with 'ok' once you have checked on it.`,
         reminderTimeoutMillis: location.reminderTimer,
         fallbackTimeoutMillis: location.fallbackTimer,
         reminderMessage: `This is a reminder to check on the bathroom`,
@@ -143,7 +154,7 @@ async function handleAlert(location, alertReason) {
       braveAlerter.sendSingleAlert(
         location.responderPhoneNumber,
         location.twilioNumber,
-        `An additional ${alertReason} alert was generated at ${location.displayName}`,
+        `An additional ${alertTypeDisplayName} alert was generated at ${location.displayName}`,
       )
     }
     await db.commitTransaction(client)
@@ -366,7 +377,7 @@ app.get('/locations/:locationId', sessionChecker, async (req, res) => {
         incidentType: recentSession.incidentType,
         id: recentSession.id,
         chatbotState: recentSession.chatbotState,
-        // alertReason: recentSession.alertReason,
+        alertType: getAlertTypeDisplayName(recentSession.alertType),
       })
     }
 
@@ -625,9 +636,9 @@ app.post('/api/sensorEvent', Validator.body(['coreid', 'event']).exists(), async
       const coreId = request.body.coreid
       const sensorEvent = request.body.event
       if (sensorEvent === SENSOR_EVENT.DURATION) {
-        alertType = ALERT_REASON.DURATION
+        alertType = ALERT_TYPE.SENSOR_DURATION
       } else if (sensorEvent === SENSOR_EVENT.STILLNESS) {
-        alertType = ALERT_REASON.STILLNESS
+        alertType = ALERT_TYPE.SENSOR_NO_MOTION
       } else {
         const errorMessage = `Bad request to ${request.path}: Invalid event type`
         helpers.logError(errorMessage)

stateMachine/StateMachine.js
@@ -1,7 +1,6 @@
-const { helpers } = require('brave-alert-lib')
+const { ALERT_TYPE, helpers } = require('brave-alert-lib')
 const stateMachineHelpers = require('./stateMachineHelpers')
 const redis = require('../db/redis')
-const ALERT_REASON = require('../AlertReasonEnum')
 const DOOR_STATUS = require('../SessionStateDoorEnum')
 const STATE = require('./SessionStateEnum')
 
@@ -47,7 +46,7 @@ async function getNextState(location, handleAlert) {
             STATE.INITIAL_TIMER,
           )
         ) {
-          await handleAlert(location, ALERT_REASON.DURATION)
+          await handleAlert(location, ALERT_TYPE.SENSOR_DURATION)
           await redis.addStateMachineData(STATE.IDLE, location.locationid)
         }
         break
@@ -57,7 +56,7 @@ async function getNextState(location, handleAlert) {
         } else if (movementOverThreshold) {
           await redis.addStateMachineData(STATE.DURATION_TIMER, location.locationid)
         } else if (await stateMachineHelpers.timerExceeded(location.locationid, parseInt(location.stillnessTimer, 10), STATE.STILLNESS_TIMER)) {
-          await handleAlert(location, ALERT_REASON.STILLNESS)
+          await handleAlert(location, ALERT_TYPE.SENSOR_NO_MOTION)
           await redis.addStateMachineData(STATE.IDLE, location.locationid)
         } else if (
           await stateMachineHelpers.timerExceeded(
@@ -66,7 +65,7 @@ async function getNextState(location, handleAlert) {
             STATE.INITIAL_TIMER,
           )
         ) {
-          await handleAlert(location, ALERT_REASON.DURATION)
+          await handleAlert(location, ALERT_TYPE.SENSOR_DURATION)
           await redis.addStateMachineData(STATE.IDLE, location.locationid)
         }
         break

test/integration/serverTest.js
@@ -6,7 +6,7 @@ const chaiDateTime = require('chai-datetime')
 const expect = chai.expect
 const { after, afterEach, before, beforeEach, describe, it } = require('mocha')
 const sinon = require('sinon')
-const { helpers } = require('brave-alert-lib')
+const { ALERT_TYPE, helpers } = require('brave-alert-lib')
 const { sleep } = require('brave-alert-lib/lib/helpers')
 const imports = require('../../index')
 
@@ -16,7 +16,6 @@ const server = imports.server
 const braveAlerter = imports.braveAlerter
 const StateMachine = require('../../stateMachine/StateMachine')
 const XETHRU_STATE = require('../../SessionStateXethruEnum')
-const ALERT_REASON = require('../../AlertReasonEnum')
 const SENSOR_EVENT = require('../../SensorEventEnum')
 
 const MOVEMENT_THRESHOLD = 40
@@ -199,7 +198,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
 
     it('should create a session with STILLNESS as the alert reason for a valid STILLNESS request', async () => {
@@ -207,7 +206,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_NO_MOTION)
     })
 
     it('should only create one new session when receiving multiple alerts within the session reset timeout', async () => {
@@ -218,7 +217,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_NO_MOTION)
     })
 
     it('should update updatedAt for the session when a new alert is received within the session reset timeout', async () => {
@@ -226,7 +225,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       const oldUpdatedAt = sessions[0].updatedAt
       await sleep(1000)
-      await firmwareAlert(radar_coreID, ALERT_REASON.STILLNESS)
+      await firmwareAlert(radar_coreID, ALERT_TYPE.SENSOR_NO_MOTION)
       const newSessions = await db.getAllSessionsFromLocation(testLocation1Id)
       const newUpdatedAt = newSessions[0].updatedAt
       expect(newUpdatedAt).to.be.afterTime(oldUpdatedAt)
@@ -240,7 +239,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(2)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
   })
 
@@ -345,7 +344,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_NO_MOTION)
     })
 
     it('radar data showing movement should start a Duration timer, if movement persists without a door opening for longer than the duration threshold, it should trigger an alert', async () => {
@@ -357,7 +356,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
   })
 
@@ -540,7 +539,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_NO_MOTION)
     })
 
     it('radar data showing movement should trigger a session, if movement persists without a door opening for longer than the duration threshold, it should trigger an alert', async () => {
@@ -552,7 +551,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
   })
 

test/unit/StateMachineTest.js
@@ -1,17 +1,19 @@
+// Third-party dependencies
 const { expect, use } = require('chai')
 const { describe, it } = require('mocha')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+
+// In-house dependencies
+const { ALERT_TYPE } = require('brave-alert-lib')
 const redis = require('../../db/redis')
-
-const testLocationId = 'TestLocation1'
-
-const ALERT_REASON = require('../../AlertReasonEnum')
 const STATE = require('../../stateMachine/SessionStateEnum')
 const DOOR_STATE = require('../../SessionStateDoorEnum')
 const StateMachine = require('../../stateMachine/StateMachine')
 const stateMachineHelpers = require('../../stateMachine/stateMachineHelpers')
 const Location = require('../../Location')
+
+const testLocationId = 'TestLocation1'
 
 // Configure Chai
 use(sinonChai)
@@ -391,7 +393,7 @@ describe('StateMachine.js unit tests: getNextState', () => {
       it('should issue a duration alert', async () => {
         const handleAlert = sinon.stub()
         await StateMachine.getNextState(new Location(testLocationId), handleAlert)
-        expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_REASON.DURATION)
+        expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_TYPE.SENSOR_DURATION)
       })
     })
   })
@@ -524,7 +526,7 @@ describe('StateMachine.js unit tests: getNextState', () => {
     it('should issue a duration alert', async () => {
       const handleAlert = sinon.stub()
       await StateMachine.getNextState(new Location(testLocationId), handleAlert)
-      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_REASON.DURATION)
+      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_TYPE.SENSOR_DURATION)
     })
   })
 
@@ -554,7 +556,7 @@ describe('StateMachine.js unit tests: getNextState', () => {
     it('should issue a stillness alert', async () => {
       const handleAlert = sinon.stub()
       await StateMachine.getNextState(new Location(testLocationId), handleAlert)
-      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_REASON.STILLNESS)
+      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_TYPE.SENSOR_NO_MOTION)
     })
   })
 })

bravetechnologycoop/BraveSensor

index.js
@@ -11,8 +11,7 @@ const Mustache = require('mustache')
 const Validator = require('express-validator')
 
 // In-house dependencies
-const { helpers } = require('brave-alert-lib')
-const ALERT_REASON = require('./AlertReasonEnum')
+const { ALERT_TYPE, helpers } = require('brave-alert-lib')
 const redis = require('./db/redis')
 const db = require('./db/db')
 const StateMachine = require('./stateMachine/StateMachine')
@@ -108,8 +107,22 @@ async function generateCalculatedTimeDifferenceString(timeToCompare) {
   return returnString
 }
 
-async function handleAlert(location, alertReason) {
-  helpers.log(`Alert for: ${location.locationid} Display Name: ${location.displayName} CoreID: ${location.radarCoreId}`)
+function getAlertTypeDisplayName(alertType) {
+  let displayName = ''
+  if (alertType === ALERT_TYPE.SENSOR_DURATION) {
+    displayName = 'Duration'
+  } else if (alertType === ALERT_TYPE.SENSOR_STILLNESS) {
+    displayName = 'Stillness'
+  } else {
+    displayName = 'Unknown'
+  }
+
+  return displayName
+}
+
+async function handleAlert(location, alertType) {
+  const alertTypeDisplayName = getAlertTypeDisplayName(alertType)
+  helpers.log(`${alertTypeDisplayName} Alert for: ${location.locationid} Display Name: ${location.displayName} CoreID: ${location.radarCoreId}`)
 
   let client
 
@@ -123,12 +136,12 @@ async function handleAlert(location, alertReason) {
     const currentTime = await db.getCurrentTime(client)
 
     if (currentSession === null || currentTime - currentSession.updatedAt >= helpers.getEnvVar('SESSION_RESET_THRESHOLD')) {
-      const newSession = await db.createSession(location.locationid, location.responderPhoneNumber, alertReason, client)
+      const newSession = await db.createSession(location.locationid, location.responderPhoneNumber, alertType, client)
       const alertInfo = {
         sessionId: newSession.id,
         toPhoneNumber: location.responderPhoneNumber,
         fromPhoneNumber: location.twilioNumber,
-        message: `This is a ${alertReason} alert. Please check on the bathroom at ${location.displayName}. Please respond with 'ok' once you have checked on it.`,
+        message: `This is a ${alertTypeDisplayName} alert. Please check on the bathroom at ${location.displayName}. Please respond with 'ok' once you have checked on it.`,
         reminderTimeoutMillis: location.reminderTimer,
         fallbackTimeoutMillis: location.fallbackTimer,
         reminderMessage: `This is a reminder to check on the bathroom`,
@@ -143,7 +156,7 @@ async function handleAlert(location, alertReason) {
       braveAlerter.sendSingleAlert(
         location.responderPhoneNumber,
         location.twilioNumber,
-        `An additional ${alertReason} alert was generated at ${location.displayName}`,
+        `An additional ${alertTypeDisplayName} alert was generated at ${location.displayName}`,
       )
     }
     await db.commitTransaction(client)
@@ -366,7 +379,7 @@ app.get('/locations/:locationId', sessionChecker, async (req, res) => {
         incidentType: recentSession.incidentType,
         id: recentSession.id,
         chatbotState: recentSession.chatbotState,
-        // alertReason: recentSession.alertReason,
+        alertType: getAlertTypeDisplayName(recentSession.alertType),
       })
     }
 
@@ -630,9 +643,9 @@ app.post('/api/sensorEvent', Validator.body(['coreid', 'event']).exists(), async
       const coreId = request.body.coreid
       const sensorEvent = request.body.event
       if (sensorEvent === SENSOR_EVENT.DURATION) {
-        alertType = ALERT_REASON.DURATION
+        alertType = ALERT_TYPE.SENSOR_DURATION
       } else if (sensorEvent === SENSOR_EVENT.STILLNESS) {
-        alertType = ALERT_REASON.STILLNESS
+        alertType = ALERT_TYPE.SENSOR_STILLNESS
       } else {
         const errorMessage = `Bad request to ${request.path}: Invalid event type`
         helpers.logError(errorMessage)

stateMachine/StateMachine.js
@@ -1,7 +1,6 @@
-const { helpers } = require('brave-alert-lib')
+const { ALERT_TYPE, helpers } = require('brave-alert-lib')
 const stateMachineHelpers = require('./stateMachineHelpers')
 const redis = require('../db/redis')
-const ALERT_REASON = require('../AlertReasonEnum')
 const DOOR_STATUS = require('../SessionStateDoorEnum')
 const STATE = require('./SessionStateEnum')
 
@@ -47,7 +46,7 @@ async function getNextState(location, handleAlert) {
             STATE.INITIAL_TIMER,
           )
         ) {
-          await handleAlert(location, ALERT_REASON.DURATION)
+          await handleAlert(location, ALERT_TYPE.SENSOR_DURATION)
           await redis.addStateMachineData(STATE.IDLE, location.locationid)
         }
         break
@@ -57,7 +56,7 @@ async function getNextState(location, handleAlert) {
         } else if (movementOverThreshold) {
           await redis.addStateMachineData(STATE.DURATION_TIMER, location.locationid)
         } else if (await stateMachineHelpers.timerExceeded(location.locationid, parseInt(location.stillnessTimer, 10), STATE.STILLNESS_TIMER)) {
-          await handleAlert(location, ALERT_REASON.STILLNESS)
+          await handleAlert(location, ALERT_TYPE.SENSOR_STILLNESS)
           await redis.addStateMachineData(STATE.IDLE, location.locationid)
         } else if (
           await stateMachineHelpers.timerExceeded(
@@ -66,7 +65,7 @@ async function getNextState(location, handleAlert) {
             STATE.INITIAL_TIMER,
           )
         ) {
-          await handleAlert(location, ALERT_REASON.DURATION)
+          await handleAlert(location, ALERT_TYPE.SENSOR_DURATION)
           await redis.addStateMachineData(STATE.IDLE, location.locationid)
         }
         break

test/integration/serverTest.js
@@ -6,7 +6,7 @@ const chaiDateTime = require('chai-datetime')
 const expect = chai.expect
 const { after, afterEach, before, beforeEach, describe, it } = require('mocha')
 const sinon = require('sinon')
-const { helpers } = require('brave-alert-lib')
+const { ALERT_TYPE, helpers } = require('brave-alert-lib')
 const { sleep } = require('brave-alert-lib/lib/helpers')
 const imports = require('../../index')
 
@@ -16,7 +16,6 @@ const server = imports.server
 const braveAlerter = imports.braveAlerter
 const StateMachine = require('../../stateMachine/StateMachine')
 const XETHRU_STATE = require('../../SessionStateXethruEnum')
-const ALERT_REASON = require('../../AlertReasonEnum')
 const SENSOR_EVENT = require('../../SensorEventEnum')
 
 const MOVEMENT_THRESHOLD = 40
@@ -202,7 +201,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
 
     it('should create a session with STILLNESS as the alert reason for a valid STILLNESS request', async () => {
@@ -210,7 +209,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_STILLNESS)
     })
 
     it('should only create one new session when receiving multiple alerts within the session reset timeout', async () => {
@@ -221,7 +220,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_STILLNESS)
     })
 
     it('should update updatedAt for the session when a new alert is received within the session reset timeout', async () => {
@@ -229,7 +228,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       const oldUpdatedAt = sessions[0].updatedAt
       await sleep(1000)
-      await firmwareAlert(radar_coreID, ALERT_REASON.STILLNESS)
+      await firmwareAlert(radar_coreID, ALERT_TYPE.SENSOR_STILLNESS)
       const newSessions = await db.getAllSessionsFromLocation(testLocation1Id)
       const newUpdatedAt = newSessions[0].updatedAt
       expect(newUpdatedAt).to.be.afterTime(oldUpdatedAt)
@@ -243,7 +242,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(2)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
   })
 
@@ -348,7 +347,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_STILLNESS)
     })
 
     it('radar data showing movement should start a Duration timer, if movement persists without a door opening for longer than the duration threshold, it should trigger an alert', async () => {
@@ -360,7 +359,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
   })
 
@@ -543,7 +542,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.STILLNESS)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_STILLNESS)
     })
 
     it('radar data showing movement should trigger a session, if movement persists without a door opening for longer than the duration threshold, it should trigger an alert', async () => {
@@ -555,7 +554,7 @@ describe('Brave Sensor server', () => {
       const sessions = await db.getAllSessionsFromLocation(testLocation1Id)
       expect(sessions.length).to.equal(1)
       const session = sessions[0]
-      expect(session.alertReason).to.equal(ALERT_REASON.DURATION)
+      expect(session.alertType).to.equal(ALERT_TYPE.SENSOR_DURATION)
     })
   })
 

test/unit/StateMachineTest.js
@@ -1,17 +1,19 @@
+// Third-party dependencies
 const { expect, use } = require('chai')
 const { describe, it } = require('mocha')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+
+// In-house dependencies
+const { ALERT_TYPE } = require('brave-alert-lib')
 const redis = require('../../db/redis')
-
-const testLocationId = 'TestLocation1'
-
-const ALERT_REASON = require('../../AlertReasonEnum')
 const STATE = require('../../stateMachine/SessionStateEnum')
 const DOOR_STATE = require('../../SessionStateDoorEnum')
 const StateMachine = require('../../stateMachine/StateMachine')
 const stateMachineHelpers = require('../../stateMachine/stateMachineHelpers')
 const Location = require('../../Location')
+
+const testLocationId = 'TestLocation1'
 
 // Configure Chai
 use(sinonChai)
@@ -391,7 +393,7 @@ describe('StateMachine.js unit tests: getNextState', () => {
       it('should issue a duration alert', async () => {
         const handleAlert = sinon.stub()
         await StateMachine.getNextState(new Location(testLocationId), handleAlert)
-        expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_REASON.DURATION)
+        expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_TYPE.SENSOR_DURATION)
       })
     })
   })
@@ -524,7 +526,7 @@ describe('StateMachine.js unit tests: getNextState', () => {
     it('should issue a duration alert', async () => {
       const handleAlert = sinon.stub()
       await StateMachine.getNextState(new Location(testLocationId), handleAlert)
-      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_REASON.DURATION)
+      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_TYPE.SENSOR_DURATION)
     })
   })
 
@@ -554,7 +556,7 @@ describe('StateMachine.js unit tests: getNextState', () => {
     it('should issue a stillness alert', async () => {
       const handleAlert = sinon.stub()
       await StateMachine.getNextState(new Location(testLocationId), handleAlert)
-      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_REASON.STILLNESS)
+      expect(handleAlert).to.be.calledWith(new Location(testLocationId), ALERT_TYPE.SENSOR_STILLNESS)
     })
   })
 })