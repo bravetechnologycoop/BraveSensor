2dde5bbf73d63224fadc2565be066a8621a2aee9
James Seto <jameseto1@gmail.com>
2021-08-04 15:31:16 -0700

    changed fields to HeartbeatData for detailed heartbeats, adapted api post to parse new heartbeats

--- db/HeartbeatData.js
+++ db/HeartbeatData.js
@@ -1,9 +1,10 @@
 class HeartbeatData {
   constructor(streamEntry) {
     this.timestamp = streamEntry[0].split('-')[0] // get timestamp in millis from redis ID
-    this.doorStatus = streamEntry[1][1]
-    this.doorTime = streamEntry[1][3]
-    this.insTime = streamEntry[1][5]
+    this.missedDoorMessages = streamEntry[1][1]
+    this.doorLowBattery = streamEntry[1][3]
+    this.doorHeartbeatReceived = streamEntry[1][5]
+    this.stateTransitions = streamEntry[1][7]
   }
 }
 
