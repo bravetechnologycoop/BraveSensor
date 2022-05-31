c888c20f602907bfc072238b8a62fd1436f3e362
PeterScholtens <peter@brave.coop>
2022-05-31 12:00:28 -0700

    Edited firmware version number

--- firmware/boron-ins-fsm/src/BraveSensorProductionFirmware.cpp
+++ firmware/boron-ins-fsm/src/BraveSensorProductionFirmware.cpp
@@ -19,7 +19,7 @@ void setup();
 void loop();
 #line 13 "/Users/peterscholtens/Desktop/BraveSensor/firmware/boron-ins-fsm/src/BraveSensorProductionFirmware.ino"
 #define DEBUG_LEVEL LOG_LEVEL_INFO
-#define BRAVE_FIRMWARE_VERSION 50600 //see versioning notes in the readme
+#define BRAVE_FIRMWARE_VERSION 50400 //see versioning notes in the readme
 #define BRAVE_PRODUCT_ID 14807 //14807 = beta units, 15479 = production units
 
 PRODUCT_ID(BRAVE_PRODUCT_ID); //you get this number off the particle console, see readme for instructions
