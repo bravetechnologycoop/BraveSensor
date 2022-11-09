6c2ffb3a902c950c032d0844a5897579c4ec081f
Kerry Zhang <kerryzhang@brave.coop>
2022-11-09 11:35:31 -0800

    Updated references to new generic IM door sensor names

--- firmware/boron-ins-fsm/src/consoleFunctions.cpp
+++ firmware/boron-ins-fsm/src/consoleFunctions.cpp
@@ -224,7 +224,7 @@ int ins_threshold_set(String input){
 // command is a long string with all the config values
 int im21_door_id_set(String command) { 
   char buffer[64];
-  IM21DoorID doorIDHolder; 
+  IMDoorID doorIDHolder; 
 
   if(isValidIM21Id(command) == false){
     return -1;
@@ -235,9 +235,9 @@ int im21_door_id_set(String command) {
 
   //if echo, publish current door ID
   if(*checkForEcho == 'e'){  
-    EEPROM.get(ADDR_IM21_DOORID, doorIDHolder.byte1);  
-    EEPROM.get((ADDR_IM21_DOORID+1), doorIDHolder.byte2);  
-    EEPROM.get((ADDR_IM21_DOORID+2), doorIDHolder.byte3);  
+    EEPROM.get(ADDR_IM_DOORID, doorIDHolder.byte1);  
+    EEPROM.get((ADDR_IM_DOORID+1), doorIDHolder.byte2);  
+    EEPROM.get((ADDR_IM_DOORID+2), doorIDHolder.byte3);  
 
     snprintf(buffer, sizeof(buffer), "{\"byte1\":\"%02X\", \"byte2\":\"%02X\", \"byte3\":\"%02X\"}", 
             doorIDHolder.byte1, doorIDHolder.byte2, doorIDHolder.byte3); 
@@ -263,9 +263,9 @@ int im21_door_id_set(String command) {
     globalDoorID.byte1 = (uint8_t)strtol(byteholder3,NULL,16);
 
     //write new global door ID to flash
-    EEPROM.put(ADDR_IM21_DOORID, globalDoorID.byte1);  
-    EEPROM.put((ADDR_IM21_DOORID+1), globalDoorID.byte2);  
-    EEPROM.put((ADDR_IM21_DOORID+2), globalDoorID.byte3); 
+    EEPROM.put(ADDR_IM_DOORID, globalDoorID.byte1);  
+    EEPROM.put((ADDR_IM_DOORID+1), globalDoorID.byte2);  
+    EEPROM.put((ADDR_IM_DOORID+2), globalDoorID.byte3); 
 
     // put door ID in buffer for return value
     snprintf(buffer, sizeof(buffer), "%02X%02X%02X", globalDoorID.byte1, globalDoorID.byte2, globalDoorID.byte3);
