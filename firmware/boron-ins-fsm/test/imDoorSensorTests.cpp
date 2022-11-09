dcc5d67ddac20b91218e273ad0cceca9fc8064f5
Kerry Zhang <kerryzhang@brave.coop>
2022-11-09 15:08:15 -0800

    Updated door sensor tests formatting, renamed to door sensor tests

---
+++ firmware/boron-ins-fsm/test/imDoorSensorTests.cpp
@@ -0,0 +1,35 @@
+#define CATCH_CONFIG_MAIN
+#include "base.h"
+#include "../src/imDoorSensor.cpp"
+#include "../src/imDoorSensor.h"
+#include "../src/flashAddresses.h"
+
+int isDoorOpen(int doorStatus) {
+    return ((doorStatus & 0x02) >> 1);
+}
+
+SCENARIO("isDoorOpen", "[isDoorOpen]") {
+    GIVEN("A variety of IM door status advertising data") {
+        int doorStatusOpen[8] = { 0x02, 0x03, 0x06, 0x07, 0x0A, 0x0B, 0x0E, 0x0F };
+        int doorStatusClosed[8] = { 0x00, 0x01, 0x04, 0x05, 0x08, 0x09, 0x0C, 0x0D };
+
+        WHEN("The function is called with the door status indicating open") {
+
+            THEN("The function should return 1") {
+                for (int i = 0; i < 8; ++i) {
+                    REQUIRE(isDoorOpen(doorStatusOpen[i]) == 1);
+                }
+            }
+        }
+
+        WHEN("The function is called with the door status indicating closed") {
+
+            THEN("The function should return 1") {
+                for (int i = 0; i < 8; ++i) {
+                    REQUIRE(isDoorOpen(doorStatusClosed[i]) == 0);
+                }
+            }
+        }
+    }
+
+}
