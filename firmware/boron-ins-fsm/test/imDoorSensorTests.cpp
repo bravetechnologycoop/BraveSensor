72da67e3acc8e68b95b0deacca091726e9d44e26
ker1200 <60584013+ker1200@users.noreply.github.com>
2022-12-05 09:54:17 -0800

    Renamed macro definition

--- firmware/boron-ins-fsm/test/imDoorSensorTests.cpp
+++ firmware/boron-ins-fsm/test/imDoorSensorTests.cpp
@@ -4,17 +4,17 @@
 #include "../src/imDoorSensor.h"
 #include "../src/flashAddresses.h"
 
-#define NO_OF_TEST_DOOR_STATUS 8
+#define NUMBER_OF_DOOR_STATUS_TEST_CASES 8
 
 SCENARIO("isDoorOpen", "[isDoorOpen]") {
     GIVEN("A variety of IM door status advertising data") {
-        int doorStatusOpen[NO_OF_TEST_DOOR_STATUS] = { 0x02, 0x03, 0x06, 0x07, 0x0A, 0x0B, 0x0E, 0x0F };
-        int doorStatusClosed[NO_OF_TEST_DOOR_STATUS] = { 0x00, 0x01, 0x04, 0x05, 0x08, 0x09, 0x0C, 0x0D };
+        int doorStatusOpen[NUMBER_OF_DOOR_STATUS_TEST_CASES] = { 0x02, 0x03, 0x06, 0x07, 0x0A, 0x0B, 0x0E, 0x0F };
+        int doorStatusClosed[NUMBER_OF_DOOR_STATUS_TEST_CASES] = { 0x00, 0x01, 0x04, 0x05, 0x08, 0x09, 0x0C, 0x0D };
 
         WHEN("The function is called with the door status indicating open") {
 
             THEN("The function should return 1") {
-                for (int i = 0; i < NO_OF_TEST_DOOR_STATUS; ++i) {
+                for (int i = 0; i < NUMBER_OF_DOOR_STATUS_TEST_CASES; ++i) {
                     REQUIRE(isDoorOpen(doorStatusOpen[i]) == 1);
                 }
             }
@@ -23,7 +23,7 @@ SCENARIO("isDoorOpen", "[isDoorOpen]") {
         WHEN("The function is called with the door status indicating closed") {
 
             THEN("The function should return 1") {
-                for (int i = 0; i < NO_OF_TEST_DOOR_STATUS; ++i) {
+                for (int i = 0; i < NUMBER_OF_DOOR_STATUS_TEST_CASES; ++i) {
                     REQUIRE(isDoorOpen(doorStatusClosed[i]) == 0);
                 }
             }
