9f5a0401d57bc7f813e4c2b3a858c21b662c3384
ker1200 <60584013+ker1200@users.noreply.github.com>
2022-12-15 09:42:04 -0800

    Added comment explaining mock eeprom implementation

--- firmware/boron-ins-fsm/test/mocks/mock_eeprom.h
+++ firmware/boron-ins-fsm/test/mocks/mock_eeprom.h
@@ -1,7 +1,14 @@
 /*
  * Mock EEPROM memory for Particle Boron
  * Reference: https://docs.particle.io/reference/device-os/api/eeprom/eeprom/
- * The Boron has 4096 bytes of emulated EEPROM.
+ * 
+ * NOTE: Due to an issue where sizeof() returns different values on different 
+ * operating systems for certain data types, this mock EEPROM implementation
+ * differs from the actual Boron EEPROM by always allocating BYTES_PER_ELEMENT
+ * bytes for each element being stored. In reality, the EEPROM only has 
+ * BORON_EEPROM_SIZE bytes of memory. When data is read from or written to the
+ * address pointed to by _address, sizeof(data) is used to read or write a
+ * number of bytes equal to the size of the data type. 
 */
 
 #pragma once
