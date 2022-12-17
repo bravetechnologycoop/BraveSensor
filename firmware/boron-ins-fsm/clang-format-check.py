0308624f3fbcf34d20c36c083d40e4a72f6e0922
ker1200 <60584013+ker1200@users.noreply.github.com>
2022-12-16 17:52:19 -0800

    Fix shell output for clang-format-check.py

--- firmware/boron-ins-fsm/clang-format-check.py
+++ firmware/boron-ins-fsm/clang-format-check.py
@@ -12,7 +12,8 @@
 import os
 
 if os.system("find src -iname *.h -o -iname *.cpp | xargs clang-format-12 --Werror --dry-run") or os.system("find test -iname *.h -o -iname *.cpp | xargs clang-format-12 --Werror --dry-run"):
-    print("Formatting error(s) found, run clang-format-all.py and try again\n")
+    os.system("echo \"Formatting error(s) found, run clang-format-all.py and try again!\n\"")
     os._exit(1)
-    
+
+os.system("echo \"No Formatting errors found!\n\"")  
 os._exit(0)
