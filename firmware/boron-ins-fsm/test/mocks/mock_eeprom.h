1284f6dc9e5bf6e73343ec0ca7c85e417a84b704
ker1200 <60584013+ker1200@users.noreply.github.com>
2022-12-21 16:49:53 -0800

    Fix endianness of returned door ID in mock eeprom

--- firmware/boron-ins-fsm/test/mocks/mock_eeprom.h
+++ firmware/boron-ins-fsm/test/mocks/mock_eeprom.h
@@ -3,8 +3,8 @@
  * Reference: https://docs.particle.io/reference/device-os/api/eeprom/eeprom/
  * 
  * The mock EEPROM is coded such that calling get() to get the IM door ID will
- * always set the data variable to 0x12 for the first byte of ADDR_IM_DOORID,
- * 0x34 for the second byte, and 0x56 for the third byte. 
+ * always set the data variable to 0x56 for the first byte of ADDR_IM_DOORID,
+ * 0x34 for the second byte, and 0x12 for the third byte. 
 */
 
 #pragma once
@@ -18,13 +18,13 @@ public:
     template <typename T>
     void get(int const _address, T& data) {
         if (_address == ADDR_IM_DOORID) {
-            data = (T)0x12;
+            data = (T)0x56;
         }
         else if (_address == ADDR_IM_DOORID + 1) {
             data = (T)0x34;
         }
         else if (_address == ADDR_IM_DOORID + 2) {
-            data = (T)0x56;
+            data = (T)0x12;
         }
     }
 
