4a2adc91c33bc9a2c33ff36d424cbeb812368ff0
Susan <susanchung-brave@users.noreply.github.com>
2023-05-04 15:19:58 -0700

    modified db.js function for debugging

--- server/db/db.js
+++ server/db/db.js
@@ -723,10 +723,10 @@ async function numberOfStillnessAlertsInIntervalOfTime(locationid, pgClient) {
       FROM sessions
       WHERE alert_type = $1
       AND locationid = $2
-      AND created_at BETWEEN NOW() - $3 * INTERVAL '1 minute'
+      AND created_at BETWEEN NOW() - $3 * INTERVAL '1 second'
       AND NOW()
       `,
-      [ALERT_TYPE.SENSOR_STILLNESS, locationid, intervalToCheckAlerts],
+      [ALERT_TYPE.SENSOR_STILLNESS, locationid, intervalToCheckAlerts * 60],
       pool,
       pgClient,
     )
