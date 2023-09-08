a230199b1e4a8c3534ddb6796c09fe79069900e5
Alex Morton <alex.ewan.morton@googlemail.com>
2023-09-08 12:01:21 +0200

    Add GH Actions for Development deployment

--- server/setup_postgresql.sh
+++ server/setup_postgresql.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-pg_password=$1
-pg_user=$2
-pg_host=$3
-pg_dbname=$4
-pg_port=$5
+
+if [[ -z "${PG_PASSWORD}" ]]; then
+  pg_password=$1
+else
+  pg_password="${PG_PASSWORD}"
+fi
+
+if [[ -z "${PG_USER}" ]]; then
+  pg_user=$2
+else
+  pg_user="${PG_USER}"
+fi
+
+if [[ -z "${PG_HOST}" ]]; then
+  pg_host=$3
+else
+  pg_host="${PG_HOST}"
+fi
+
+if [[ -z "${PG_DATABASE}" ]]; then
+  pg_dbname=$4
+else
+  pg_dbname="${PG_DATABASE}"
+fi
+
+if [[ -z "${PG_PORT}" ]]; then
+  pg_port=$5
+else
+  pg_port="${PG_PORT}"
+fi
 
 for file in $(ls -v db/*.sql); do
   echo "Running script $file"
