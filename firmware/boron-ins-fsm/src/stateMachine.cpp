1b6fd8fd9983068862fa933c4eb67f807bdd0412
ahmaddaadaa <ahmadd@uvic.ca>
2023-10-04 14:32:54 -0700

    gets the INS sensors values properly

--- firmware/boron-ins-fsm/src/stateMachine.cpp
+++ firmware/boron-ins-fsm/src/stateMachine.cpp
@@ -379,10 +379,6 @@ const char *resetReasonString(int resetReason) {
 
 void getHeartbeat() {
     static unsigned long lastHeartbeatPublish = 0;
-    
-    // Check INS value
-    filteredINSData checkINS = checkINS3331();
-    bool isINSZero = (checkINS.iAverage < 0.0001);
 
     // 1st "if condition" is so that the boron publishes a heartbeat on startup
     // 2nd "if condition" is so that the boron publishes a heartbeat, when the doorMessageReceivedFlag is true.
@@ -391,14 +387,18 @@ void getHeartbeat() {
     // 3rd "if condition" is true only if a heartbeat hasnt been published in the last SM_HEARTBEAT_INTERVAL
     if (lastHeartbeatPublish == 0 || (doorMessageReceivedFlag && ((millis() - doorHeartbeatReceived) >= HEARTBEAT_PUBLISH_DELAY)) ||
         (millis() - lastHeartbeatPublish) > SM_HEARTBEAT_INTERVAL) {
+        // Check INS value
+        filteredINSData checkINS = checkINS3331();
+        bool isINSZero = (checkINS.iAverage < 0.0001);
         static unsigned int didMissQueueSum = 0;
         static std::queue<bool> didMissQueue;
         // from particle docs, max length of publish is 622 chars, I am assuming this includes null char
         char heartbeatMessage[622] = {0};
         JSONBufferWriter writer(heartbeatMessage, sizeof(heartbeatMessage) - 1);
         writer.beginObject();
-        // Add "is_ins_zero" field to the JSON message
-        writer.name("is_ins_zero").value(isINSZero);
+      
+        // Add "isINSZero" field to the JSON message
+        writer.name("isINSZero").value(isINSZero);
 
         if (didMissQueue.size() > SM_HEARTBEAT_DID_MISS_QUEUE_SIZE) {
             // if oldest value did miss; subtract from the current amount
