30e54ee1ab2751c1ba925615dfbe1037c60a44cc
Tara MacKinnon <taramackinnon4@gmail.com>
2023-11-13 16:20:26 -0800

    remove uneccessary spy, add rollback test case to all describe blocks

--- server/test/integration/dbTest/beginTransactionTest.js
+++ server/test/integration/dbTest/beginTransactionTest.js
@@ -19,11 +19,14 @@ describe('db.js integration tests: beginTransaction', () => {
   /* eslint-disable no-underscore-dangle */
   let poolConnectStub
   let pgClient
+  let rollbackTransactionStub
 
   beforeEach(() => {
     sandbox.spy(helpers, 'logError')
     sandbox.spy(helpers, 'log')
     poolConnectStub = sandbox.stub()
+    rollbackTransactionStub = sandbox.stub()
+    db.__set__('rollbackTransaction', rollbackTransactionStub)
     db.__set__('pool', { connect: poolConnectStub })
   })
 
@@ -35,13 +38,16 @@ describe('db.js integration tests: beginTransaction', () => {
     let clientStub
     beforeEach(async () => {
       clientStub = { query: sandbox.stub() }
-      sandbox.spy(db, 'beginTransaction')
       poolConnectStub.onCall(0).rejects(new Error('deadlock detected')).onCall(1).resolves(clientStub)
       pgClient = await db.beginTransaction()
     })
 
     it('should log the deadlock error', () => {
       expect(helpers.logError).to.be.calledWith(`Error running the runBeginTransactionWithRetries query: Error: deadlock detected`)
+    })
+
+    it('should not try and rollback the transaction', () => {
+      expect(rollbackTransactionStub).to.not.be.called
     })
 
     it('should log the retry', () => {
@@ -61,13 +67,16 @@ describe('db.js integration tests: beginTransaction', () => {
     let clientStub
     beforeEach(async () => {
       clientStub = { query: sinon.stub() }
-      sandbox.spy(db, 'beginTransaction')
       poolConnectStub.onCall(0).resolves(clientStub)
       pgClient = await db.beginTransaction()
     })
 
     it('should not log any errors', () => {
       expect(helpers.logError).to.not.be.called
+    })
+
+    it('should not try and rollback the transaction', () => {
+      expect(rollbackTransactionStub).to.not.be.called
     })
 
     it('should not log a retry', () => {
@@ -85,13 +94,9 @@ describe('db.js integration tests: beginTransaction', () => {
 
   describe('when beginTransaction fails twice, deadlocking on the first attempt and throwing an error with a valid pgClient on the second attempt', () => {
     let clientStub
-    let rollbackTransactionStub
     beforeEach(async () => {
-      rollbackTransactionStub = sandbox.stub()
-      db.__set__('rollbackTransaction', rollbackTransactionStub)
       clientStub = { query: sandbox.stub() }
       clientStub.query.withArgs('BEGIN').rejects(new Error('some error'))
-      sandbox.spy(db, 'beginTransaction')
       poolConnectStub.onCall(0).rejects(new Error('deadlock detected')).onCall(1).resolves(clientStub)
       pgClient = await db.beginTransaction()
     })
