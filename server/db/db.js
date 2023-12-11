b13551a817f7a38936aa0a176a2e263103cdf2c3
Tara MacKinnon <taramackinnon4@gmail.com>
2023-12-11 15:48:51 -0800

    Fix typo with pg pool

--- server/db/db.js
+++ server/db/db.js
@@ -13,8 +13,7 @@ const pool = new pg.Pool({
   user: helpers.getEnvVar('PG_USER'),
   database: helpers.getEnvVar('PG_DATABASE'),
   password: helpers.getEnvVar('PG_PASSWORD'),
-  // ssl: { rejectUnauthorized: false },
-  ssl: false,
+  ssl: { rejectUnauthorized: false },
 })
 
 // 1114 is OID for timestamp in Postgres
