322e4608c8d639f3b5b7fd0faae2579a455f8ade
Darrickg <darrickgunawan@gmail.com>
2024-05-22 12:09:45 -0700

    fixed typo in sentry alert and removed unecessary comments

--- server/vitals.js
+++ server/vitals.js
@@ -197,13 +197,12 @@ async function sendLowBatteryAlert(locationid) {
   }
 }
 
-// FIXME: the spanish message is google translated, needs fixing
 async function sendFallOffAlert(location) {
   if (!location.client.isSendingVitals || !location.isSendingVitals) {
     return
   }
 
-  helpers.logSentry(`Sending an door sensor fallOff alert for ${location.locationid}`)
+  helpers.logSentry(`Sending a door sensor fallOff alert for ${location.locationid}`)
 
   await sendSingleAlert(
     location.locationid,
