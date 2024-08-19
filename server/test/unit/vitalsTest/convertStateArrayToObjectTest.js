9f4331a9c77a43d8d18288551eeab792a7362686
Johnny Liu <johnny@brave.coop>
2024-08-19 14:26:23 -0700

    server test fix

--- server/test/unit/vitalsTest/convertStateArrayToObjectTest.js
+++ server/test/unit/vitalsTest/convertStateArrayToObjectTest.js
@@ -20,7 +20,7 @@ describe('vitals.js unit tests: convertStateArrayToObject', () => {
       const stateTransitionsArrayReadable = [
         {
           state: 'idle',
-          reason: 'movement',
+          reason: 'low_movement',
           time: 1,
         },
       ]
