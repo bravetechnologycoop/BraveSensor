ca20535fe31e0080abbf62d63ed1270399d019d2
Denis <dlondry@foxsmithtech.com>
2024-10-15 20:17:28 -0700

    error handling clean-up of i2c interface

--- firmware/curie/src/i2cInterface.cpp
+++ firmware/curie/src/i2cInterface.cpp
@@ -67,7 +67,7 @@ int i2cInterface::closeBus(){
 
 int i2cInterface::readBytes(uint8_t slaveAddr, uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data){
     bDebug(TRACE, "i2c readBytes");
-    int err = 0;
+    int err = OK;
     char cmd[2] = {(char)(startAddress >> 8), (char)(startAddress & 0xFF)};
     char buf[1664];
     uint16_t *p = data;
@@ -80,60 +80,62 @@ int i2cInterface::readBytes(uint8_t slaveAddr, uint16_t startAddress, uint16_t n
         err = this->openBus();
     }
 
-    i2c_messages[0].addr = slaveAddr;
-    i2c_messages[0].flags = 0;
-    i2c_messages[0].len = 2;
-    i2c_messages[0].buf = (I2C_MSG_FMT *)cmd;
+    if (!err){
+        i2c_messages[0].addr = slaveAddr;
+        i2c_messages[0].flags = 0;
+        i2c_messages[0].len = 2;
+        i2c_messages[0].buf = (I2C_MSG_FMT *)cmd;
 
-    i2c_messages[1].addr = slaveAddr;
-    i2c_messages[1].flags = I2C_M_RD | I2C_M_NOSTART;
-    i2c_messages[1].len = nMemAddressRead * 2;
-    i2c_messages[1].buf = (I2C_MSG_FMT *)buf;
+        i2c_messages[1].addr = slaveAddr;
+        i2c_messages[1].flags = I2C_M_RD | I2C_M_NOSTART;
+        i2c_messages[1].len = nMemAddressRead * 2;
+        i2c_messages[1].buf = (I2C_MSG_FMT *)buf;
 
-    i2c_messageset[0].msgs = i2c_messages;
-    i2c_messageset[0].nmsgs = 2;
+        i2c_messageset[0].msgs = i2c_messages;
+        i2c_messageset[0].nmsgs = 2;
 
-    memset(buf, 0, nMemAddressRead * 2);
+        memset(buf, 0, nMemAddressRead * 2);
 
-    if (ioctl(this->fileI2C, I2C_RDWR, &i2c_messageset) < 0) 
-    {
-        bDebug(ERROR, "i2c read error");
-        return -1;
-    }
-
-    for (int count = 0; count < nMemAddressRead; count++) 
-    {
-        int i = count << 1;
-        *p++ = ((uint16_t)buf[i] << 8) | buf[i + 1];
+        if (ioctl(this->fileI2C, I2C_RDWR, &i2c_messageset) < 0) 
+        {
+            bDebug(ERROR, "i2c read error");
+            err = FILE_ERROR;
+        } else {
+            for (int count = 0; count < nMemAddressRead; count++) 
+            {
+                int i = count << 1;
+                *p++ = ((uint16_t)buf[i] << 8) | buf[i + 1];
+            }
+        }
     }
 
     return err;
 
 }
 
-//!!! change to single return
 int i2cInterface::writeBytes(uint8_t slaveAddr, uint16_t writeAddress, uint16_t data){
     bDebug(TRACE, "i2c writeBytes");
-    int err = 0;
+    int err = FILE_ERROR;
     char cmd[4] = {(char)(writeAddress >> 8), (char)(writeAddress & 0x00FF), (char)(data >> 8), (char)(data & 0x00FF)};
 
     struct i2c_msg i2c_messages[1];
     struct i2c_rdwr_ioctl_data i2c_messageset[1];
 
-    // !!! check that we have the file handle before doing anything
+    if (this->fileI2C){
+        err = OK;
+        i2c_messages[0].addr = slaveAddr;
+        i2c_messages[0].flags = 0;
+        i2c_messages[0].len = 4;
+        i2c_messages[0].buf = (I2C_MSG_FMT *)cmd;
 
-    i2c_messages[0].addr = slaveAddr;
-    i2c_messages[0].flags = 0;
-    i2c_messages[0].len = 4;
-    i2c_messages[0].buf = (I2C_MSG_FMT *)cmd;
+        i2c_messageset[0].msgs = i2c_messages;
+        i2c_messageset[0].nmsgs = 1;
 
-    i2c_messageset[0].msgs = i2c_messages;
-    i2c_messageset[0].nmsgs = 1;
-
-    if (ioctl(this->fileI2C, I2C_RDWR, &i2c_messageset) < 0) 
-    {
-        bDebug(ERROR, "I2C Write Error");
-        return -1;
+        if (ioctl(this->fileI2C, I2C_RDWR, &i2c_messageset) < 0) 
+        {
+            bDebug(ERROR, "I2C Write Error");
+            err = FILE_ERROR;
+        }
     }
 
     return err;
