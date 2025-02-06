212315e3ee3f121da10604d8e48ed79c617c0c2a
Nakul Bansal <nakulbansal727@gmail.com>
2025-02-06 14:05:46 -0800

    Enabled validation

--- server/src/routes.js
+++ server/src/routes.js
@@ -35,8 +35,7 @@ function configureRoutes(app) {
 
   app.post('/api/sensorEvent', events.validateSensorEvent, events.handleSensorEvent)
   app.post('/api/heartbeat', vitals.validateHeartbeat, vitals.handleHeartbeat)
-  // app.post('/alert/sms', events.validateTwilioRequest, events.handleTwilioRequest)
-  app.post('/alert/sms', events.handleTwilioRequest)
+  app.post('/alert/sms', events.validateTwilioRequest, events.handleTwilioRequest)
 
   app.post('/pa/get-google-tokens', pa.validateGetGoogleTokens, pa.getGoogleTokens)
   app.post('/pa/get-google-payload', pa.validateGetGooglePayload, pa.getGooglePayload)
