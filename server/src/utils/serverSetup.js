2b3300932d91381a604c515fdd48ecc92cda0270
Corey Cheng <corey@brave.coop>
2025-04-17 14:45:17 -0700

    Fixed serverSetup

--- server/src/utils/serverSetup.js
+++ server/src/utils/serverSetup.js
@@ -11,7 +11,6 @@ const https = require('https')
 // In-house dependencies
 const helpers = require('./helpers')
 const vitals = require('../vitals')
-// const teamsHelpers = require('./teamsHelpers')
 
 const checkDisconnectionIntervalinSeconds = helpers.getEnvVar('CHECK_DEVICE_DISCONNECTION_INTERVAL')
 
@@ -20,10 +19,6 @@ function setupServer(app) {
 
   if (helpers.isTestEnvironment()) {
     server = app.listen(8000)
-    // setup checks for device disconnection
-
-    // const card = teamsHelpers.createAdaptiveCard('teamsDurationAlert', { surveyCategories: 'ABC,XYZ' }, { displayName: 'Washroom XYZ' })
-    // console.log(JSON.stringify(card, null, 2))
   } else {
     const httpsOptions = {
       key: fs.readFileSync(`/etc/brave/ssl/tls.key`),
@@ -34,6 +29,15 @@ function setupServer(app) {
 
     // setup sentry monitoring
     helpers.setupSentry(app, helpers.getEnvVar('SENTRY_DSN'), helpers.getEnvVar('ENVIRONMENT'), helpers.getEnvVar('RELEASE'))
+
+    // setup checks for device disconnection
+    setInterval(async () => {
+      try {
+        await vitals.checkDeviceDisconnectionVitals()
+      } catch (error) {
+        helpers.logError(`SERVER: Error checking device connection vitals: ${error.message}`)
+      }
+    }, checkDisconnectionIntervalinSeconds * 1000)
   }
 
   return server
