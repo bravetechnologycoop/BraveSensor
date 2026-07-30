2efc5abfb91fc1ffaf91cb955d4741a469efe727
CCheng00 <32044608+CCheng00@users.noreply.github.com>
2026-07-30 15:49:02 -0700

    Cc twilio received time (#401)
    * Surface the time that Twilio received the message in the session details page.
    
    * CI

--- server/src/models/Event.js
+++ server/src/models/Event.js
@@ -1,11 +1,14 @@
 class Event {
-  constructor(eventId, sessionId, eventType, eventTypeDetails, eventSentAt, phoneNumbers) {
+  constructor(eventId, sessionId, eventType, eventTypeDetails, eventSentAt, phoneNumbers, receivedAt = null) {
     this.eventId = eventId
     this.sessionId = sessionId
     this.eventType = eventType
     this.eventTypeDetails = eventTypeDetails
     this.eventSentAt = eventSentAt
     this.phoneNumbers = phoneNumbers
+    // When this outbound SMS was actually delivered to the handset (Twilio delivery callback),
+    // as opposed to eventSentAt which is when we handed it to Twilio. Null until/unless delivered.
+    this.receivedAt = receivedAt
   }
 }
 
